c7453b3652876c4738694ac72c54408539e07db5
Kim Hee Su <wlskaka4@gmail.com>
2023-11-12 16:52:17 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 151 ms, Memory: 33,588 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -5,44 +5,62 @@ public class Solution {
 	static int T;
 	static int V,E, a,b;
 	static StringTokenizer st;
-	static int parents[];
+	static int parents[][];
 	
 	static int depth[];
 	
-	static int dfs(int cur, int parent) {
+	static int dfs(int cur) {
 		int ret = 1;
 		for(int nxt : adj[cur]) {
-			if(nxt == parent)
+			if(visited[nxt])
 				continue;
 			depth[nxt] = depth[cur] + 1;
-			ret += dfs(nxt, cur);
+			parents[0][nxt] = cur;
+			visited[nxt] = true;
+			ret += dfs(nxt);
 		}
 		return ret;
 	}
 	
 	static int LCA(int a,int b) {
-		dfs(1,-1);
+		if(depth[a] < depth[b])
+			return LCA(b,a);
 		
-		
-		while(depth[a] != depth[b]) {
-			if(depth[a] > depth[b])
-				a = parents[a];
-			else
-				b = parents[b];
+		for(int h = MAX_H; h>=0; h--) {
+			if(depth[a] - depth[b] >= (1<<h))
+				a = parents[h][a];
 		}
 		
-		while(a != b) {
-			a = parents[a];
-			b = parents[b];
+		if(a == b)
+			return a;
+		
+		for(int h = MAX_H;h>=0;h--) {
+			if(parents[h][a] != parents[h][b]) {
+				a = parents[h][a];
+				b = parents[h][b];
+			}
 		}
 		
-		return a;
-//		return dfs(1, -1);
+		return parents[0][a];
 	}
 	static ArrayList<Integer> adj[];
-	static int MAX_V = 10003;
+	static int MAX_H = 16;
+	static boolean visited[];
+	
+	static void initialize() {
+		parents = new int[MAX_H+1][V+1];
+		depth = new int[V+1];
+		visited = new boolean[V+1];
+		visited[1] = true;
+		dfs(1);
+		
+		for(int h = 1; h<=MAX_H; h++) {
+			for(int i = 1; i<=V;i++)
+				parents[h][i] = parents[h-1][parents[h-1][i]];
+		}
+	}
 	public static void main(String[] args) throws IOException{
-		// System.setIn(new FileInputStream("./input.txt"));
+		//System.setIn(new FileInputStream("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
@@ -53,29 +71,31 @@ public class Solution {
 			E = Integer.parseInt(st.nextToken());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
-			parents = new int[MAX_V+1];
-			depth = new int[MAX_V+1];
 			
-			adj = new ArrayList[MAX_V+1];
-			for(int i = 1; i<=MAX_V;i++) {
+			
+			adj = new ArrayList[V+1];
+			for(int i = 1; i<=V;i++) {
 				adj[i] = new ArrayList<>();
 			}
 			
-			parents[1] = -1;
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < E; i++) {
 				int p = Integer.parseInt(st.nextToken());
 				int child = Integer.parseInt(st.nextToken());
-				parents[child] = p;
 				adj[p].add(child);
 				adj[child].add(p);
 			}
+			
+			initialize();
 			
 			sb.append('#').append(tc).append(' ');
 			
 			int lca = LCA(a,b);
 //			System.out.println(lca);
-			int size = dfs(lca, parents[lca]);
+			Arrays.fill(visited, false);
+			int parent = parents[0][lca];
+			visited[parent] = true;
+			int size = dfs(lca) - 1;
 			sb.append(lca).append(' ').append(size).append('\n');
 		}
 		
