5402c75a4916ca3f95117a3c7969fd9e2f9fd608
Kim Hee Su <wlskaka4@gmail.com>
2023-12-20 16:33:56 +0900

    [Silver IV] Title: 스택, Time: 132 ms, Memory: 16728 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,66 +1,90 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	static int n;
-	static int MAX_N = 10000;
+
+class Node{
+	Node prev, next;
+	int value;
+}
+
+
+class Stack{
+	Node top;
+	int size;
 	
-	static StringTokenizer st;
-	
-	static class Node{
-		Node prev, next;
-		int value;
+	public Stack() {
+		top = null;
+		size = 0;
 	}
 	
-	static Node top = null;
-	static int size = 0;
-	static Node getNewNode(int num) {
+	Node getNewNode(int num) {
 		Node node = new Node();
 		node.value = num;
 		return node;
 	}
+	
+	void push(int num) {
+		Node node = getNewNode(num);
+		if(top == null) {
+			top = node;
+		}else {
+			top.next = node;
+			node.prev = top;
+			top = node;
+		}
+		size++;
+	}
+	
+	int pop() {
+		if(top == null)
+			return -1;
+		else {
+			size--;
+			int ret = top.value;
+			top = top.prev;
+			return ret;
+		}
+	}
+	
+	int getSize() {
+		return size;
+	}
+	
+	boolean isEmpty() {
+		return top == null;
+	}
+	
+	int getTop() {
+		return top.value;
+	}
+}
+
+
+public class Main {
+	static int n;
+	
+	static StringTokenizer st;
 	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
+		Stack stk = new Stack();
 		for(int i = 0; i < n ; i++) {
 			st = new StringTokenizer(br.readLine());
 			String op = st.nextToken();
 			if(op.equals("push")) {
 				int num = Integer.parseInt(st.nextToken());
-				Node node = getNewNode(num);
-				if(top == null) {
-					top = node;
-				}else {
-					top.next = node;
-					node.prev = top;
-					top = node;
-				}
-				size++;
+				stk.push(num);
 			}else if(op.equals("pop")) {
-				if(top == null) {
-//					System.out.println(-1);
-					sb.append(-1).append('\n');
-				}else {
-					size--;
-					int ret = top.value;
-					top = top.prev;
-//					System.out.println(ret);
-					sb.append(ret).append('\n');
-				}
-				
+				sb.append(stk.pop()).append('\n');
 			}else if(op.equals("size")) {
-//				System.out.println(size);
-				sb.append(size).append('\n');
+				sb.append(stk.getSize()).append('\n');
 			}else if(op.equals("empty")) {
-//				System.out.println(top==null ? 1 : 0);
-				sb.append(top == null ? 1 : 0).append('\n');
+				sb.append(stk.isEmpty() ? 1 : 0).append('\n');
 			}else if(op.equals("top")){
-//				System.out.println(top == null ? -1 : top.value);
-				sb.append(top == null ? -1 : top.value).append('\n');
+				sb.append(stk.isEmpty() ? -1 : stk.getTop()).append('\n');
 			}
 		}
 		
