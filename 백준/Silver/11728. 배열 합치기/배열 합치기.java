760b9369955062e402f09038c6a3df117187bcb5
Kim Hee Su <wlskaka4@gmail.com>
2023-12-22 20:50:52 +0900

    [Silver V] Title: 배열 합치기, Time: 1268 ms, Memory: 369664 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,60 +1,55 @@
-import java.io.*;
-/**
- * dp
- */
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/11728
 public class Main {
-	static int n,m;
-	static StringTokenizer st;
-	static int a[], b[], arr[];
-	public static void main(String[] args) throws IOException{
-	 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	 	st = new StringTokenizer(br.readLine());
-	 	n = Integer.parseInt(st.nextToken());
-	 	m = Integer.parseInt(st.nextToken());
-	 	a = new int[n+1];
-	 	b = new int[m+1];
-	 	st = new StringTokenizer(br.readLine());
-	 	for(int i = 1;i<=n; i++) {
-	 		a[i] = Integer.parseInt(st.nextToken());
-	 	}
-	 	
-	 	st = new StringTokenizer(br.readLine());
-	 	for(int i = 1;i<=m; i++) {
-	 		b[i] = Integer.parseInt(st.nextToken());
-	 	}
-	 	Arrays.sort(a,1,n+1);
-	 	Arrays.sort(b,1,m+1);
-	 	
-	 	arr = new int[n+m+1];
-	 	int r = 1;
-	 	int idx = 1;
-	 	int l = 1;
-	 	for(l =1; l<=n; l++) {
-	 		while(r <= m && b[r] < a[l]) {
-	 			arr[idx] = b[r];
-	 			idx++;
-	 			r++;
-	 		}
-	 		
-	 		arr[idx] = a[l];
-	 		idx++;
-	 	}
-//	 	System.out.printf("l: %d, r: %d, idx: %d\n", l,r,idx);
-//	 	System.out.println(Arrays.toString(arr));
-	 	if( r <= m) {
-	 		for(int i = idx; i<=n+m;i++) {
-	 			arr[i] = b[r];
-	 			r++;
-	 		}
-	 	}
-//	 	System.out.println("----");
-//	 	System.out.printf("l: %d, r: %d, idx: %d\n", l,r,idx);
-//	 	System.out.println(Arrays.toString(arr));
-	 	StringBuilder sb = new StringBuilder();
-	 	for(int i = 1; i<=n+m;i++) {
-	 		sb.append(arr[i]).append(' ');
-	 	}
-	 	System.out.println(sb);
-	}
+    static int n,m;
+    static int[] a;
+    static int[] b;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n + 1];
+        b = new int[m + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =1 ;i<=n;i++)
+            a[i] = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i =1 ; i<=m;i++)
+            b[i] = Integer.parseInt(st.nextToken());
+
+        int left = 1, right = 1;// 두 포인터 초기화, left는 a에서, right는 b에서
+        while (left <= n && right <= m) { // left와 right가 모두 범위내에 있다면
+            if (a[left] <= b[right]) { // a의 현재값이 b의 현재값보다 작다면
+                sb.append(a[left] + " ");
+                left += 1;
+            } else {
+                sb.append(b[right] + " ");
+                right += 1;
+            }
+        }
+
+
+        while (left <= n) { // right가 범위밖이지만 left는 범위안일 때
+            sb.append(a[left] + " ");
+            left += 1;
+        }
+
+        while (right <= m) {
+            sb.append(b[right] + " ");
+            right += 1;
+        }
+
+        System.out.println(sb);
+
+    }
 }
