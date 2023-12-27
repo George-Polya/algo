57c33facedd56758792d3f72e6f48989c7c2833b
Kim Hee Su <wlskaka4@gmail.com>
2023-12-27 11:05:18 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 20376 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,43 +1,44 @@
-
-
-import java.util.*;
 import java.io.*;
-
-public class Main{
-	static StringTokenizer st;
+import java.util.*;
+/**
+ * 중복없이 m개를 골라서 줄을 세운다 
+ * nCm * m! = 순
+ * 비트마스킹을 이용해서 풀어보자 
+ */
+public class Main {
 	static int n,m;
-	static ArrayList<Integer> selected = new ArrayList<>();
-	static boolean visited[];
+	static int arr[];
+	static StringTokenizer st;
+	static int selected[];
 	static StringBuilder sb = new StringBuilder();
-	static void solve(int cnt) {
+	static void solve(int cnt, int bit) {
 		if(cnt == m) {
-			for(int i = 0; i<selected.size();i++)
-				sb.append(selected.get(i)).append(" ");
-			sb.append("\n");
+			for(int i = 0; i < m ;i++) {
+				sb.append(selected[i]).append(' ');
+			}
+			sb.append('\n');
 			return;
-			
 		}
 		
-		
-		for(int i = 1; i<=n; i++) {
-			if(visited[i])
+		for(int i =0; i < n ;i++) {
+			if(((bit >> i) & 1) == 1)
 				continue;
-			visited[i] = true;
-			selected.add(i);
-			solve(cnt+1);
-			visited[i] = false;
-			selected.remove(selected.size() - 1);
+			selected[cnt] = arr[i];
+			solve(cnt + 1, bit | (1 << i));
 		}
 		
 	}
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		visited = new boolean[n+1];
-		
-		solve(0);
-		System.out.println(sb);
+	 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 	st = new StringTokenizer(br.readLine());
+	 	n = Integer.parseInt(st.nextToken());
+	 	m = Integer.parseInt(st.nextToken());
+	 	arr = new int[n];
+	 	for(int i = 0;i < n ;i++) {
+	 		arr[i] = i + 1;
+	 	}
+	 	selected = new int[m];
+	 	solve(0, 0);
+	 	System.out.println(sb);
 	}
 }
