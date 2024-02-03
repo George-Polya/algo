3c1d3b7e7f41afca81e36f58430c7ebd2beb0d66
Kim Hee Su <wlskaka4@gmail.com>
2024-02-03 11:28:50 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 150 ms, Memory: 31,648 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -1,104 +1,92 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Solution {
+
 	static int T;
-	static int V,E, a,b;
+	static int v,e,a,b;
 	static StringTokenizer st;
-	static int parents[][];
+	static int MAX_V = 10000;
+	static List<Integer> adj[] = new List[MAX_V+5];
+//	static int LCA(int a, int b) {
+//		
+//	}
 	
-	static int depth[];
-	
-	static int dfs(int cur) {
-		int ret = 1;
+	static int parent[] = new int[MAX_V+5];
+	static int depth[] = new int[MAX_V+5];
+	static void dfs(int cur) {
 		for(int nxt : adj[cur]) {
-			if(visited[nxt])
+			if(nxt == parent[cur])
 				continue;
+			parent[nxt] = cur;
 			depth[nxt] = depth[cur] + 1;
-			parents[0][nxt] = cur;
-			visited[nxt] = true;
-			ret += dfs(nxt);
+			dfs(nxt);
+		}
+	}
+	
+	static int LCA(int a, int b) {
+		while(depth[a] != depth[b]) {
+			if(depth[a] < depth[b])
+				b = parent[b];
+			else if(depth[a] > depth[b])
+				a = parent[a];
+		}
+		
+		while(a != b) {
+			a = parent[a];
+			b = parent[b];
+		}
+		return a;
+	}
+	
+	static int getSubtreeSize(int cur) {
+		int ret = 1;
+		
+		for(int nxt : adj[cur]) {
+			if(nxt == parent[cur])
+				continue;
+			ret += getSubtreeSize(nxt);
 		}
 		return ret;
 	}
 	
-	static int LCA(int a,int b) {
-		if(depth[a] < depth[b])
-			return LCA(b,a);
-		
-		for(int h = MAX_H; h>=0; h--) {
-			if(depth[a] - depth[b] >= (1<<h))
-				a = parents[h][a];
-		}
-		
-		if(a == b)
-			return a;
-		
-		for(int h = MAX_H;h>=0;h--) {
-			if(parents[h][a] != parents[h][b]) {
-				a = parents[h][a];
-				b = parents[h][b];
-			}
-		}
-		
-		return parents[0][a];
-	}
-	static ArrayList<Integer> adj[];
-	static int MAX_H = 16;
-	static boolean visited[];
-	
-	static void initialize() {
-		parents = new int[MAX_H+1][V+1];
-		depth = new int[V+1];
-		visited = new boolean[V+1];
-		visited[1] = true;
-		dfs(1);
-		
-		for(int h = 1; h<=MAX_H; h++) {
-			for(int i = 1; i<=V;i++)
-				parents[h][i] = parents[h-1][parents[h-1][i]];
-		}
-	}
 	public static void main(String[] args) throws IOException{
-		//System.setIn(new FileInputStream("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
 		for(int tc=1;tc<=T;tc++) {
 			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
+			v = Integer.parseInt(st.nextToken());
+			e = Integer.parseInt(st.nextToken());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			
 			
-			adj = new ArrayList[V+1];
-			for(int i = 1; i<=V;i++) {
+			for(int i =1;i<=v;i++) {
 				adj[i] = new ArrayList<>();
 			}
 			
+			parent[1] = -1;
+			depth[1] = 0;
+			
 			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < E; i++) {
-				int p = Integer.parseInt(st.nextToken());
-				int child = Integer.parseInt(st.nextToken());
-				adj[p].add(child);
-				adj[child].add(p);
+			for(int i = 0; i <e ; i++) {
+				int c = Integer.parseInt(st.nextToken());
+				int d = Integer.parseInt(st.nextToken());
+				adj[c].add(d);
+				adj[d].add(c);
 			}
 			
-			initialize();
-			
-			sb.append('#').append(tc).append(' ');
+			dfs(1);
 			
 			int lca = LCA(a,b);
-//			System.out.println(lca);
-			Arrays.fill(visited, false);
-			int parent = parents[0][lca];
-			visited[parent] = true;
-			int size = dfs(lca) - 1;
-			sb.append(lca).append(' ').append(size).append('\n');
+			int cnt = getSubtreeSize(lca);
+//			int cnt = 0;
+			
+			sb.append('#').append(tc).append(' ').append(lca).append(' ').append(cnt).append('\n');
+			
 		}
-		
 		System.out.println(sb);
 	}
+	
 }
