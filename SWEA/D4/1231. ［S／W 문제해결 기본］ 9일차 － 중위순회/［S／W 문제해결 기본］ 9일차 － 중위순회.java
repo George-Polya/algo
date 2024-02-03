a61e63af575e359fd891a30ab819613fd89f15ef
Kim Hee Su <wlskaka4@gmail.com>
2024-02-03 10:53:22 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 110 ms, Memory: 19,408 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,51 +1,50 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Solution {
 	static int T = 10;
-	static StringTokenizer st;
 	static int n;
-	static int lefts[], rights[];
-	static char tree[];
-	static StringBuilder order;
+	static int MAX_N = 100;
+	static char arr[] = new char[MAX_N+5];
+	static int node[] = new int[2 * MAX_N + 5];
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
 	
 	static void inOrder(int cur) {
-		int left = lefts[cur];
-		int right = rights[cur];
+		if(node[cur * 2] != 0){
+			inOrder(cur * 2);
+		}
 		
-		if(tree[left] != '#')
-			inOrder(left);
-		order.append(tree[cur]);
+		sb.append(arr[cur]);
 		
-		if(tree[right]!='#')
-			inOrder(right);
-		
+		if(node[cur * 2 + 1] != 0) {
+			inOrder(cur * 2 + 1);
+		}
 	}
+	
 	public static void main(String[] args) throws IOException{
-//		System.setIn(new FileInputStream("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
-		for(int tc=1; tc<=T;tc++) {
+		for(int tc=1;tc<=T;tc++) {
 			n = Integer.parseInt(br.readLine());
-			lefts = new int[n + 1];
-			rights = new int[n+1];
-			tree = new char[2 * n +3];
-			Arrays.fill(tree, '#');
-			for(int i = 1; i<=n; i++) {
+			
+			for(int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				int cur = Integer.parseInt(st.nextToken());
+				int id = Integer.parseInt(st.nextToken());
+				node[id * 2] = 0;
+				node[id * 2+1] = 0;
 				char ch = st.nextToken().charAt(0);
-				tree[cur] = ch;
+				arr[id] = ch;
 				if(st.hasMoreTokens())
-					lefts[cur] = Integer.parseInt(st.nextToken());
+					node[id * 2] = Integer.parseInt(st.nextToken());
 				if(st.hasMoreTokens())
-					rights[cur] = Integer.parseInt(st.nextToken());
+					node[id * 2 + 1] = Integer.parseInt(st.nextToken());
 			}
-			order = new StringBuilder();
+			
+			sb.append("#").append(tc).append(' ');
 			inOrder(1);
-			sb.append("#").append(tc).append(' ').append(order).append('\n');
+			sb.append('\n');
 		}
 		System.out.println(sb);
 	}
+	
 }
