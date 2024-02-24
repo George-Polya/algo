5c41e38099e8e03ec736e9894496a92ff40489c5
Kim Hee Su <wlskaka4@gmail.com>
2024-02-24 11:15:18 +0900

    [D3] Title: 0/1 Knapsack, Time: 134 ms, Memory: 19,892 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,51 +1,61 @@
 import java.io.*;
 import java.util.*;
-
 public class Solution {
 	static int T;
-	static StringTokenizer st;
-	
 	static int n,k;
-	static int dp[][];
-	static int value[], weight[];
+	static int MAX_N = 100;
+	static int MAX_K = 1000;
+	static int dp[];
 	static int INT_MIN = Integer.MIN_VALUE;
+	static StringTokenizer st;
+	static int volumes[];
+	static int values[];
+	static void init() {
+		for(int i = 0; i<=n; i++)
+			dp[i] = INT_MIN;
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		
 		for(int tc=1;tc<=T;tc++) {
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			k = Integer.parseInt(st.nextToken());
+			volumes = new int[n+1];
+			values = new int[n+1];
+			dp = new int[k+1];
 			
-			dp = new int[n+1][k+1];
-			
-			value = new int[n+1];
-			weight = new int[n+1];
-			
-			for(int i = 1; i<=n; i++) {
+			for(int i = 0; i< n; i++) {
 				st = new StringTokenizer(br.readLine());
-				weight[i] = Integer.parseInt(st.nextToken());
-				value[i] = Integer.parseInt(st.nextToken());
+				volumes[i] = Integer.parseInt(st.nextToken());
+				values[i] = Integer.parseInt(st.nextToken());
 			}
 			
+			init();
+			dp[0] = 0;
 			
-			for(int i = 1; i<=n;i++) {
-				for(int j = 1; j<=k; j++) {
-					dp[i][j] = dp[i-1][j];
+			for(int i = 0; i < n ;i++) {
+				for(int j = k; j>=volumes[i]; j--) {
 					
-					if(j - weight[i] >= 0)
-						dp[i][j] = Math.max(dp[i][j], dp[i-1][j-weight[i]] + value[i]);
+					if(dp[j - volumes[i]] == INT_MIN)
+						continue;
+					dp[j] = Math.max(dp[j], dp[j-volumes[i]] + values[i]);
+				
 				}
 			}
 			
-			
+//			System.out.println(Arrays.toString(dp));
 			int ans = INT_MIN;
-			for(int i = 1; i<=k;i++)
-				ans = Math.max(ans, dp[n][i]);
+			for(int i = 0; i<= k;i++)
+				ans = Math.max(ans, dp[i]);
+			sb.append('#').append(tc).append(' ').append(ans).append('\n');			
 			
-			sb.append("#").append(tc).append(' ').append(ans).append('\n');
+			
 		}
 		System.out.println(sb);
 	}
+	
+	
 }
