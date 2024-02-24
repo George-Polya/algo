7103f3fe668d1bd312d127d62bddffcf755eae93
Kim Hee Su <wlskaka4@gmail.com>
2024-02-24 12:27:30 +0900

    [D4] Title: 중간값 구하기, Time: 1,151 ms, Memory: 106,556 KB -BaekjoonHub

--- SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
+++ SWEA/D4/3000. 중간값 구하기/중간값 구하기.java
@@ -1,56 +1,54 @@
 import java.io.*;
 import java.util.*;
-
 public class Solution {
 	static int T;
-	static int n,a;
 	static StringTokenizer st;
-	static PriorityQueue<Integer> maxH;
-	static PriorityQueue<Integer> minH;
+	static int n,a;
+	static PriorityQueue<Integer> minHeap;
+	static PriorityQueue<Integer> maxHeap;
 	static int MOD = 20171109;
 	public static void main(String[] args) throws IOException{
-//		System.setIn(new FileInputStream("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		for(int tc=1;tc<=T;tc++) {
-			maxH = new PriorityQueue<>(Collections.reverseOrder());
-			minH = new PriorityQueue<>();
 			st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			a = Integer.parseInt(st.nextToken());
-			minH.add(a);
-			int answer = 0;
+			
+			minHeap = new PriorityQueue<>();
+			maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+			minHeap.add(a);
+			
+			int result = 0;
 			for(int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
-				int value1 = Integer.parseInt(st.nextToken());
-				int value2 = Integer.parseInt(st.nextToken());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				int min = Math.min(x, y);
+				int max = Math.max(x,y);
 				
-				if(value1 < value2) {
-					maxH.add(value1);
-					minH.add(value2);
-				}else {
-					maxH.add(value2);
-					minH.add(value1);
+				minHeap.add(max);
+				maxHeap.add(min);
+				
+				if(minHeap.peek() < maxHeap.peek()) {
+					min = minHeap.poll();
+					max = maxHeap.poll();
+					minHeap.add(max);
+					maxHeap.add(min);
 				}
 				
-				while(minH.peek() < maxH.peek()) {
-					int max = maxH.poll();
-					int min = minH.poll();
-					
-					maxH.add(min);
-					minH.add(max);
-				}
-				
-				answer = (minH.peek() + answer) % MOD;
+				result += minHeap.peek() % MOD;
+				result %= MOD;
 			}
 			
+//			int result = minHeap.peek();
 			
-			sb.append('#').append(tc).append(' ').append(answer).append('\n');
-			
-			
-		
+			sb.append('#').append(tc).append(' ').append(result).append('\n');
 		}
 		System.out.println(sb);
+				
 	}
+	
+	
 }
