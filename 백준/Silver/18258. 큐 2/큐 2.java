af78fb3e5483f020b8077bde9ac2acc9a1054e27
Kim Hee Su <wlskaka4@gmail.com>
2024-05-08 19:50:19 +0900

    [Silver IV] Title: 큐 2, Time: 1248 ms, Memory: 328940 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -5,11 +5,47 @@ import java.io.*;
 
 public class Main {
 	static int n;
+	static int MAX_N = 2000000; 
 	static StringTokenizer st;
+	static class Queue{
+		int arr[] = new int[MAX_N+1];
+		int back, front;
+		
+		public Queue() {
+			this.back = this.front = 0;
+		}
+		
+		public void offer(int x) {
+			arr[back] = x;
+			back++;
+		}
+		
+		public int poll() {
+			return arr[front++];
+		}
+		
+		public int size() {
+			return back - front;
+		}
+		
+		public boolean isEmpty() {
+			return back==front;
+		}
+		
+		public int peek() {
+			return arr[front];
+		}
+		
+		public int peekLast() {
+//			System.out.println("back" + back);
+			return arr[back-1];
+		}
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		LinkedList<Integer> q = new LinkedList<>();
+//		LinkedList<Integer> q = new LinkedList<>();
+		Queue q = new Queue();
 		StringBuilder sb = new StringBuilder();
 		for(int turn=1; turn<=n; turn++) {
 			st = new StringTokenizer(br.readLine());
