84df4c73a371e6186fe92477f90415cf872856ea
Kim Hee Su <wlskaka4@gmail.com>
2024-05-15 14:13:00 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 29052 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -0,0 +1,57 @@
+// https://www.acmicpc.net/problem/2110
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, c;
+    static int arr[];
+
+    static boolean check(int dist) {
+        int limit = -1;
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (limit <= arr[i]) {
+                limit = dist + arr[i];
+                cnt += 1;
+            }
+        }
+
+        return c <= cnt;
+    }
+
+    static int lower_bound(int arr[], int target) {
+        int st = 1, en = 1000000000;
+        int result = 0;
+        while (st <= en) {
+            int mid = (st+en)/ 2;
+
+            if (check(mid)) {
+                result = mid;
+                st = mid + 1;
+            } else {
+                en = mid-1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr, 1, n + 1);
+        int result = lower_bound(arr, c);
+        System.out.println(result);
+    }
+}
