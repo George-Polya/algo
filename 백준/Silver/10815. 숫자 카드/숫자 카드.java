b3e24ef2ac8060755470d65bff44c16e13828528
Kim Hee Su <wlskaka4@gmail.com>
2024-06-03 14:40:37 +0900

    [Silver V] Title: 숫자 카드, Time: 848 ms, Memory: 173080 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,46 +4,25 @@ import java.io.*;
  * 2) 이분탐색 << 이걸로 해보자 
  */
 import java.util.*;
-public class Main {
-	static int arr[];
+public class Main{
 	static int n,m;
+	static HashSet<Integer> set = new HashSet<>();
 	static StringTokenizer st;
-	
-	static boolean binary_search(int num) {
-		int l = 1;
-		int r = n;
-		
-		while(l<=r) {
-			int mid = (l + r)/2;
-			
-			if(arr[mid]== num)
-				return true;
-			else if(arr[mid] < num) {
-				l = mid + 1;
-			}else if(arr[mid] > num) {
-				r = mid - 1;
-			}
-		}
-		return false;
-	}
-	
 	public static void main(String[] args) throws IOException{
 	 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	 	n = Integer.parseInt(br.readLine());
-	 	arr = new int[n+1];
 	 	st = new StringTokenizer(br.readLine());
-	 	for(int i = 1; i<=n; i++) {
-	 		arr[i] = Integer.parseInt(st.nextToken());
+	 	for(int i = 0; i < n ;i++) {
+	 		int num = Integer.parseInt(st.nextToken());
+	 		set.add(num);
 	 	}
-	 	
-	 	Arrays.sort(arr,1,n+1);
 	 	
 	 	m = Integer.parseInt(br.readLine());
 	 	st = new StringTokenizer(br.readLine());
 	 	StringBuilder sb = new StringBuilder();
-	 	for(int i = 0; i < m; i++) {
+	 	for(int i = 0; i < m ;i++) {
 	 		int num = Integer.parseInt(st.nextToken());
-	 		sb.append(binary_search(num) ? 1 : 0).append(' ');
+	 		sb.append(set.contains(num) ? 1 : 0).append(' ');
 	 	}
 	 	System.out.println(sb);
 	}
