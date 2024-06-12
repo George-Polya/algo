afbbb23482928576a586a3bf3d9cbc1289efad80
Kim Hee Su <wlskaka4@gmail.com>
2024-06-12 16:13:03 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.java
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.java
@@ -1,33 +1,30 @@
-import java.io.*;
 import java.util.*;
-
-public class Main{
-	
+import java.io.*;
+public class Main {
+	static long arr[];
 	static long k;
-	static long len[];
+	
 	static int solve(long n) {
 		if(n == 1)
 			return 0;
 		
-		for(int i = 0; i < 65; i++) {
-			if(n <= len[i]) {
-				return 1 - solve(n - len[i-1]);
-			}
+		for(int i = 1; i<64;i++) {
+			if(n <= arr[i])
+				return 1 - solve(n - arr[i-1]);
 		}
-		
 		return 0;
-		
 	}
+	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		k = Long.parseLong(br.readLine());
 		
-		len = new long[65];
-		len[0] = 1;
+		arr = new long[64+1];
+		arr[0] = 1;
 		for(int i = 1; i < 65;i++) {
-			len[i] = len[i-1] * 2;
+			arr[i] = 2 * arr[i-1];
 		}
-		
+			
 		System.out.println(solve(k));
 		
 	}
