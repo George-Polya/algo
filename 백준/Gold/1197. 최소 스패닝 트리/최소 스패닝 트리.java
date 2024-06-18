ef00cb0762a0fb5630f9bb0a095692e75fe3d6f9
Kim Hee Su <wlskaka4@gmail.com>
2024-06-18 12:57:34 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 756 ms, Memory: 50612 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	
+	static int v,e;
+	static class Edge implements Comparable<Edge>{
+		int u,v;
+		long cost;
+		public Edge(int u,int v,long cost) {
+			this.u = u;
+			this.v = v;
+			this.cost = cost;
+		}
+		
+		public int compareTo(Edge e) {
+			return Long.compare(cost,e.cost);
+		}
+	}
+	static StringTokenizer st;
+	static Edge edges[];
+	static int uf[];
+	
+	static int find(int x) {
+		if(uf[x] == x)
+			return x;
+		return uf[x] = find(uf[x]);
+	}
+	
+	static boolean union(int x,int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x == y)
+			return false;
+		uf[x] = y;
+		return true;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		v = Integer.parseInt(st.nextToken());
+		e = Integer.parseInt(st.nextToken());
+		edges = new Edge[e];
+		uf = new int[v+1];
+		for(int i =1 ;i<=v;i++)
+			uf[i] = i;
+		for(int i = 0; i<e;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long cost = Long.parseLong(st.nextToken());
+			edges[i] = new Edge(a,b,cost);
+		}
+		
+		Arrays.sort(edges);
+		
+		int sum = 0;
+		int cnt = 0;
+		for(int i = 0; i<e;i++) {
+			int u = edges[i].u;
+			int v = edges[i].v;
+			
+			u = find(u);
+			v = find(v);
+			
+			if(u == v)
+				continue;
+			uf[v] = u;
+			cnt++;
+			sum += edges[i].cost;
+//			if(cnt == v-1)
+//				break;
+			
+		}
+		System.out.println(sum);
+	}
+}
