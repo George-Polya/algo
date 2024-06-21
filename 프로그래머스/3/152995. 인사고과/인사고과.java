b6d601116a19ab8d00beb1f697090ee164a9c384
Kim Hee Su <wlskaka4@gmail.com>
2024-06-21 16:31:14 +0900

    [level 3] Title: 인사고과, Time: 124.15 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,44 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-        int ans = 1;
-        
-        int atti = scores[0][0];
-        int value = scores[0][1];
-        
-        List<int[]> list = new ArrayList<>();
-        for(int i = 1; i< scores.length;i++){
-            if(atti< scores[i][0] && value < scores[i][1])
-                return -1;
-            
-            if((atti + value) < scores[i][0] + scores[i][1]){
-                list.add(new int[]{scores[i][0] ,scores[i][1]});
-                ans++;
-            }
-                
-        }
-        
-        Collections.sort(list, (int[] s1, int[] s2)->{
-           return (s1[0] + s1[1]) - (s2[0] + s2[1]); 
+        int[] s = scores[0];
+        Arrays.sort(scores, (s1, s2) -> {
+            if(s1[0] != s2[0])
+                return s2[0] - s1[0];
+            return s1[1] - s2[1];
         });
         
-        for(int i = 0; i < list.size()-1;i++){
-            int a1 = list.get(i)[0];
-            int v1 = list.get(i)[1];
-            
-            for(int j = i+1;j<list.size();j++){
-                int a2 = list.get(j)[0];
-                int v2 = list.get(j)[1];
-                
-                if(a1 < a2 && v1 < v2){
-                    ans--;
-                    break;
-                }
+        
+        int max = 0, ans = 1;
+        for(int[] score : scores) {
+            if(score[1] < max) {
+                if(score.equals(s))
+                    return -1;
+            }
+            else {
+                max = Math.max(max, score[1]);
+                if(s[0] + s[1] < score[0] + score[1])
+                    ans++;
             }
         }
-        
-       
-        
         return ans;
     }
 }
