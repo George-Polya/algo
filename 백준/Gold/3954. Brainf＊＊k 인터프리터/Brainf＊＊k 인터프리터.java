1e1e55acfbcfc2771694d8fd183ec8ae721dbe8e
Kim Hee Su <wlskaka4@gmail.com>
2024-08-03 17:59:06 +0900

    [Gold I] Title: Brainf**k 인터프리터, Time: 4136 ms, Memory: 25788 KB -BaekjoonHub

--- 백준/Gold/3954. Brainf＊＊k 인터프리터/Brainf＊＊k 인터프리터.java
+++ 백준/Gold/3954. Brainf＊＊k 인터프리터/Brainf＊＊k 인터프리터.java
@@ -2,31 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+	static int n,m,k,T;
+	static int arr[];
+	static String codes, inputs;
+	static StringTokenizer st;
+	static int MOD = 256;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		int T = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
 		for(int t=0; t<T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());//배열(메모리) 크기
-			int M = Integer.parseInt(st.nextToken());//명령 = 코드 크기
-			int K = Integer.parseInt(st.nextToken());//입력 = 문자열
-			int arr[] = new int[N];//배열(메모리)
-			int p = 0, inp=0;//arr'spointer & input's pointer
-			char[] codes = br.readLine().toCharArray();
-			char[] inputs = br.readLine().toCharArray();
+			n = Integer.parseInt(st.nextToken());//배열(메모리) 크기
+			m = Integer.parseInt(st.nextToken());//명령 = 코드 크기
+			k = Integer.parseInt(st.nextToken());//입력 = 문자열
+			arr = new int[n];//배열(메모리)
+			int p = 0, inp=0;
+			codes = br.readLine();
+			inputs = br.readLine();
 			
 			//괄호 위치 저장
-			int stoe[] = new int[M];
-			int etos[] = new int[M];
-			Stack<Integer> stack = new Stack<>();
-			for(int i=0; i<M; i++) {
-				if(codes[i]=='[') {stack.push(i);}
-				else if(codes[i]==']') {
-					int pos = stack.pop();
+			int stoe[] = new int[m];
+			int etos[] = new int[m];
+			Stack<Integer> stk = new Stack<>();
+			for(int i=0; i<m; i++) {
+				if(codes.charAt(i) == '[') {
+					stk.push(i);
+				}
+				else if(codes.charAt(i)==']') {
+					int pos = stk.pop();
 					stoe[pos] = i;
 					etos[i] = pos;
 				}
@@ -35,20 +40,20 @@ public class Main {
 			//명령어 실행
 			int cnt = 0;//명령수행횟수
 			int result = 0;
-			for(int i=0; i<M; i++) {
+			for(int i=0; i<m; i++) {
 				
-				switch(codes[i]) {
+				switch(codes.charAt(i)) {
 				case '+':
-					arr[p] = (arr[p]+1)%256;
+					arr[p] = (arr[p]+1) % MOD;
 					break;
 				case '-':
-					arr[p] = (arr[p]+255)%256;
+					arr[p] = (arr[p]+255) % MOD;
 					break;
 				case '<':
-					p = (p-1+N)%N;
+					p = (p-1+n)%n;
 					break;
 				case '>':
-					p = (p+1)%N;
+					p = (p+1)%n;
 					break;
 				case '[':
 					if(arr[p]==0) {//루프 안 해도된다.
@@ -61,12 +66,12 @@ public class Main {
 						if(cnt>50_000_000) {
 							result = Math.max(result, i);
 						}
-						i=etos[i];//시작 위치로!
+						i = etos[i];//시작 위치로!
 					}
 					break;
 				case ',':
 					//입력이 더이상 없을 때
-					arr[p]=(inp>=K)?255:inputs[inp++];
+					arr[p] = (inp>=k) ? 255 : inputs.charAt(inp++);
 					break;
 				default:
 					continue;
@@ -76,6 +81,8 @@ public class Main {
 					break;
 				}
 			}
+			
+			
 			//출력
 			if(cnt>50_000_000) {
 				sb.append("Loops ").append(etos[result]).append(" ").append(result).append("\n");
