2be4f6944ea0a0a0902fb0ca3f5ba7008c325f1e
Kim Hee Su <wlskaka4@gmail.com>
2024-08-09 17:07:16 +0900

    [Gold II] Title: 보석 도둑, Time: 1672 ms, Memory: 131460 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,44 +1,55 @@
+import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-
-        List<long[]> jewels = new ArrayList<>();
-        long[] bags = new long[k];
-
-        for (int i = 0; i < n; i++) {
-            long weight = scanner.nextLong();
-            long value = scanner.nextLong();
-            jewels.add(new long[]{weight, value});
-        }
-
-        for (int i = 0; i < k; i++) {
-            bags[i] = scanner.nextLong();
-        }
-
-        // 보석과 가방을 각각 정렬
-        jewels.sort(Comparator.comparingLong(a -> a[0]));
-        Arrays.sort(bags);
-
-        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        int st = 0;
-        long ret = 0;
-
-        for (int i = 0; i < k; i++) {
-            while (st < n && jewels.get(st)[0] <= bags[i]) {
-                pq.add(jewels.get(st)[1]);
-                st++;
-            }
-            if (!pq.isEmpty()) {
-                ret += pq.poll();
-            }
-        }
-
-        System.out.println(ret);
-    }
+	static int n,k;
+	static class Jewel implements Comparable<Jewel>{
+		int weight, value;
+		public Jewel(int weight, int value) {
+			this.weight = weight;
+			this.value = value;
+		}
+		public int compareTo(Jewel jewel) {
+			return weight - jewel.weight;
+		}
+	}
+	static PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+	static StringTokenizer st;
+	static int bags[];
+	static Jewel jewels[];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		jewels = new Jewel[n];
+		for(int i = 0 ; i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int weight = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			jewels[i] = new Jewel(weight, value);
+		}
+		
+		bags = new int[k];
+		for(int i = 0; i < k;i++) {
+			bags[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(jewels);
+		Arrays.sort(bags);
+		
+		int idx = 0;
+		long ans = 0;
+		for(int i = 0; i < k ;i++) {
+			while(idx <n && jewels[idx].weight <= bags[i]) {
+				pq.add((long)jewels[idx].value);
+				idx++;
+			}
+			
+			if(!pq.isEmpty()) {
+				ans += pq.poll();
+			}
+		}
+		System.out.println(ans);
+	}
 }
