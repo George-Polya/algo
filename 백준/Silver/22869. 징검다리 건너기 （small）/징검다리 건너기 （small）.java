1c7092eb0eb431099681b84526ace334f4393335
Kim Hee Su <wlskaka4@gmail.com>
2024-08-14 10:36:25 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 232 ms, Memory: 19988 KB -BaekjoonHub

---
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static StringTokenizer st;
+	static int arr[];
+	static int n,k;
+	static boolean dp[];
+	static int INT_MAX = Integer.MAX_VALUE;
+	static int calc(int j, int i) {
+		return (j - i) * ( 1 + Math.abs(arr[i] - arr[j]));
+	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n+1];
+        for(int i = 1; i<=n; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp = new boolean[n+1];
+        dp[1] = true;
+        for(int i = 2; i<=n; i++) {
+        	boolean flag = false;
+        	for(int c = 1; c<i; c++) {
+        		if(!dp[c])
+        			continue;
+        		flag |= dp[c] && calc(i,c) <= k;
+         	}
+        	dp[i] = flag;
+        }
+        
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[n] ? "YES": "NO");
+        
+    }
+}
