33fdeca05f964e2ffd8eb9a49e60af4a72ff8dc7
Kim Hee Su <wlskaka4@gmail.com>
2024-08-30 19:41:06 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,11 @@
+select B.title,
+       B.board_id,
+       R.reply_id,
+       R.writer_id,
+       R.contents,
+       date_format(R.created_date,"%Y-%m-%d") as created_date
+from used_goods_board as B
+join used_goods_reply as R
+on B.board_id = R.board_id
+where date_format(B.created_date,"%Y-%m")= '2022-10'
+order by R.created_date asc, B.title asc;
