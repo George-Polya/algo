48721c346af6d9d2666a3eedb2e835ef0fb1ae45
Kim Hee Su <wlskaka4@gmail.com>
2024-09-02 14:30:12 +0900

    [Gold III] Title: 데스노트, Time: 148 ms, Memory: 19800 KB -BaekjoonHub

--- 백준/Gold/2281. 데스노트/데스노트.java
+++ 백준/Gold/2281. 데스노트/데스노트.java
@@ -6,23 +6,29 @@ public class Main {
     static int[] arr;
     static int[][] dp;
     static StringTokenizer st;
+    static int INT_MAX = Integer.MAX_VALUE;
     
-    static int solve(int idx, int len) {
-    	if(idx == n)
+    static int solve(int depth, int len) {
+//    	System.out.printf("%d, %d\n", depth,len);
+    	if(depth == n)
     		return 0;
     	
-    	if(dp[idx][len] != -1)
-    		return dp[idx][len];
+    	if(dp[depth][len] != INT_MAX)
+    		return dp[depth][len];
     	
-    	int left = m - len + 1;
-    	int ans = solve(idx + 1, arr[idx] + 1) + left * left;
     	
-    	if(len + arr[idx] <= m) {
-    		ans = Math.min(ans,  solve(idx + 1, len + arr[idx] + 1));
+    	
+    	if(len + arr[depth] <= m) {
+    		dp[depth][len] = Math.min(dp[depth][len],  solve(depth + 1, len + arr[depth] + 1));
     	}
     	
-    	dp[idx][len] = ans;
-    	return ans;
+    	
+    	int left = m - len + 1;
+    	dp[depth][len] = Math.min(dp[depth][len], solve(depth + 1, arr[depth] + 1) + left * left);
+    	
+    	
+    	
+    	return dp[depth][len];
     }
     public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,7 +43,7 @@ public class Main {
     		arr[i] = Integer.parseInt(br.readLine());
     	}
     	for(int i = 0; i <=n; i++) {
-    		Arrays.fill(dp[i], -1);
+    		Arrays.fill(dp[i], INT_MAX);
     	}
     	
     	System.out.println(solve(1, arr[0] + 1));
