eae4911041ecd579f5c9b9d8961e81e62391992f
Kim Hee Su <wlskaka4@gmail.com>
2024-09-28 13:34:46 +0900

    [level 2] Title: 타겟 넘버, Time: 109.61 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,22 @@
 class Solution {
-    int n;
-    int arr[];
-    int target;
     
-    int dfs(int cur, int sum){
-        if(cur == n ){
+    public int solution(int[] numbers, int target) {
+        int n = numbers.length;
+        int answer = 0;
+        for(int bit = 0; bit < (1 << n); bit++){
+            int sum = 0;
+            
+            for(int i = 0; i < n; i++){
+                if( (bit & (1<< i)) != 0)
+                   sum += numbers[i];
+                else
+                   sum -= numbers[i];
+            }
+                   
             if(sum == target)
-                return 1;
-            return 0;
+                   answer++;
         }
-        
-        int value = arr[cur];
-        return dfs(cur +1 ,sum + value) + dfs(cur +1 ,sum - value);
-    }
-    
-    public int solution(int[] numbers, int _target) {
-        n = numbers.length;
-        arr = numbers;
-        target = _target;
-        int answer = dfs(0,0);
+                   
         return answer;
     }
 }
