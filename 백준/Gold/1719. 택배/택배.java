fa525e54206c05777828555892ce069957306a58
Kim Hee Su <wlskaka4@gmail.com>
2024-10-21 12:24:06 +0900

    [Gold III] Title: 택배, Time: 332 ms, Memory: 23408 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -9,110 +9,73 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         
         trace = new int[n+1][n+1];
+        prev = new int[n+1][n+1];
+        adj = new int[n+1][n+1];
         
-        adj = new ArrayList[n+1];
-        for(int i = 1; i<=n; i++) {
-        	adj[i] = new ArrayList<>();
+        for(int y=1; y<=n; y++) {
+        	for(int x=1; x<=n; x++) {
+        		adj[y][x] = INF;
+        		trace[y][x] = x;
+        		if(y==x) {
+        			adj[y][x] = 0;
+        			trace[y][x] = 0;
+        		}
+        	}
         }
         
-        
-        for(int i = 0; i < m ;i++) {
+        for(int i = 0; i < m; i++) {
         	st = new StringTokenizer(br.readLine());
         	int from = Integer.parseInt(st.nextToken());
         	int to = Integer.parseInt(st.nextToken());
         	int cost = Integer.parseInt(st.nextToken());
-        	
-        	adj[from].add(new Edge(to, cost));
-        	adj[to].add(new Edge(from,cost));
+        	adj[from][to] = cost;
+        	adj[to][from] = cost;
         }
         
         
-        for(int i = 1; i<=n; i++) {
-        	solve(i);
-        }
-        
-        
-        StringBuilder sb= new StringBuilder();
-        for(int y=1; y<=n; y++) {
-        	for(int x=1; x<=n; x++) {
-        		sb.append(y == x ? "-" : trace[y][x]).append(' ');
+        for(int z = 1; z<=n; z++) {
+        	for(int y=1; y<=n; y++) {
+        		for(int x=1; x<=n; x++) {
+        			if(adj[y][x] > adj[y][z] + adj[z][x]) {
+        				prev[y][x] = z;
+        				adj[y][x] = adj[y][z] + adj[z][x];
+        			}
+        		}
         	}
-        	sb.append('\n');
         }
-        System.out.println(sb);
+        
+//        printBoard(prev);
+        
+        for(int idx = 1; idx<=n; idx++) {
+        	
+        	for(int i = 1; i<=n; i++) {
+        		if(idx == i)
+        			continue;
+        		int prv = i;
+        		while(prev[idx][prv] != 0)
+        			prv = prev[idx][prv];
+        		trace[idx][i] = prv;
+        	}
+        	
+        }
+        
+        printBoard(trace);
         
     }
     
-    static void solve(int idx) {
-    	PriorityQueue<Edge> pq = new PriorityQueue<>();
-    	int dist[] = new int[n+1];
-    	Arrays.fill(dist, INF);
-    	dist[idx] = 0;
-    	pq.add(new Edge(idx, dist[idx]));
-    	
-    	int prev[] = new int[n+1];
-    	
-    	
-    	while(!pq.isEmpty()) {
-//    		System.out.printf("pq: %s, dist: %s\n", pq, Arrays.toString(dist));
-    		Edge cur = pq.poll();
-    		
-    		if(dist[cur.idx] < cur.cost)
-    			continue;
-    		
-    		for(Edge nxt : adj[cur.idx]) {
-    			if(dist[nxt.idx] > dist[cur.idx] + nxt.cost) {
-    				dist[nxt.idx] = dist[cur.idx] + nxt.cost;
-    				prev[nxt.idx] = cur.idx;
-    				pq.add(new Edge(nxt.idx, dist[nxt.idx]));
-    			}
-    		}
-    	}
-//    	System.out.println(Arrays.toString(prev));
-    	
-    	
-    	for(int i = 1; i<=n; i++) {
-    		if(idx == i)
-    			continue;
-    		int prv = i;
-    		while(prev[prv] != idx) {
-    			prv = prev[prv];
-    		}
-    		trace[idx][i] = prv;
-    	}
-    	
-    }
-    
-    static ArrayList<Edge> adj[];
-    static class Edge implements Comparable<Edge>{
-    	int idx, cost;
-    	public Edge(int idx,int cost) {
-    		this.idx = idx;
-    		this.cost = cost;
-    	}
-    	public int compareTo(Edge o) {
-    		return cost - o.cost;
-    	}
-    	
-    	public String toString() {
-    		return idx+" "+cost;
-    	}
-    }
-    
-    static boolean visited[];
-    static int INF = Integer.MAX_VALUE;
+    static int INF = Integer.MAX_VALUE / 2;
     static void printBoard(int board[][]) {
+    	StringBuilder sb= new StringBuilder();
     	for(int y=1; y<=n; y++) {
     		for(int x=1; x<=n; x++) {
-    			if(board[y][x] == INF)
-    				System.out.print("-\t");
-    			else
-    				System.out.print(board[y][x]+"\t");
+    			sb.append(y == x ? "-" : board[y][x]).append(' ');
     		}
-    		System.out.println();
+    		sb.append('\n');
     	}
+    	System.out.println(sb);
     }
+    
     static StringTokenizer st;
     static int n,m;
-    static int trace[][];
+    static int trace[][], adj[][], prev[][];
 }
