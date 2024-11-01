1598d9c75e32d99e528dad53cf10680ce3355009
Kim Hee Su <wlskaka4@gmail.com>
2024-11-01 12:16:22 +0900

    [Gold IV] Title: 사이클 게임, Time: 676 ms, Memory: 244412 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[m+1][2];
+		for(int turn = 1; turn<=m; turn++) {
+			st = new StringTokenizer(br.readLine());
+			arr[turn][0] = Integer.parseInt(st.nextToken());
+			arr[turn][1] = Integer.parseInt(st.nextToken());
+		}
+		uf = new int[n];
+		for(int i = 0; i<n;i++) {
+			uf[i] = i;
+		}
+		
+		
+		for(int turn = 1; turn<=m; turn++) {
+			int i = arr[turn][0];
+			int j = arr[turn][1];
+			
+			int I = find(i);
+			int J = find(j);
+			
+			if(I == J) {
+				System.out.println(turn);
+				return;
+			}
+			
+			uf[I] = J;
+		}
+		System.out.println(0);
+	}
+	
+	static int find(int x) {
+		if(x == uf[x])
+			return x;
+		return uf[x] = find(uf[x]);
+	}
+	
+	static int uf[];
+	static int n,m;
+	static int arr[][];
+	static StringTokenizer st;
+	
+}
