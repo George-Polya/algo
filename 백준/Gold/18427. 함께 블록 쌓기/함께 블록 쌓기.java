a5c7c879650cafd2af2d4b7230e80884c39462b6
Kim Hee Su <wlskaka4@gmail.com>
2024-11-05 21:33:33 +0900

    [Gold IV] Title: 함께 블록 쌓기, Time: 140 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
+++ 백준/Gold/18427. 함께 블록 쌓기/함께 블록 쌓기.java
@@ -1,45 +1,78 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static final int MOD = 10007;
-    static int N, M, H;
-    static int[][] dp;
-    static List<Integer>[] blocks;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        blocks = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            blocks[i] = new ArrayList<>();
-            st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens()) {
-                blocks[i].add(Integer.parseInt(st.nextToken()));
-            }
-        }
-
-        dp = new int[N + 1][H + 1];
-        for (int i = 0; i <= N; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        System.out.println(solve(1, 0));
-    }
-
-    static int solve(int i, int h) {
-        if (h == H) return 1;
-        if (i > N || h > H) return 0;
-        if (dp[i][h] != -1) return dp[i][h];
-
-        int result = solve(i + 1, h) % MOD;
-        for (int block : blocks[i]) {
-            result = (result + solve(i + 1, h + block)) % MOD;
-        }
-        return dp[i][h] = result;
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n+1][m];
+		for(int i = 1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int size = st.countTokens();
+			for(int j = 0; j < size;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+//			System.out.println(Arrays.toString(arr[i]));
+		}
+		
+		dp = new int[n+1][h+1];
+		for(int row[] : dp) {
+			Arrays.fill(row, -1);
+		}
+		
+		ans = solve(1,0);
+		System.out.println(ans);
+	}
+	
+	static int dp[][];
+	
+	static int solve(int cur, int sum) {
+		if(sum == h)
+			return 1;
+		if(cur > n || sum > h)
+			return 0;
+        
+		if(dp[cur][sum] != -1)
+			return dp[cur][sum];
+		
+		int ret = 0;
+		for(int i=0; i< m; i++) {
+			if(arr[cur][i] != 0)
+				ret = (ret + solve(cur + 1, sum + arr[cur][i])) % mod;;
+				
+		}
+		ret = (ret +  solve(cur + 1, sum)) % mod;
+		return dp[cur][sum] = ret;
+	}
+	
+//	static void solve(int cur, int sum) {
+//		if(sum == h) {
+//			ans++;
+//			return;
+//		}
+//		
+//		
+//		if(cur == n + 1)
+//			return;
+//		
+//		if(sum > h)
+//			return;
+//		
+//		for(int i = 0; i < m ;i++) {
+//			if(arr[cur][i] != 0) {
+//				solve(cur + 1, sum + arr[cur][i]);
+//			}
+//		}
+//		solve(cur + 1, sum);
+//	}
+	
+	static int mod = 10007;
+	static int ans;
+	static int n,m,h;
+	static StringTokenizer st;
+	static int arr[][];
 }
