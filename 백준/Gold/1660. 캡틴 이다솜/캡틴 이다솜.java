defa558559eaa2b71aa2eba074c4476886e9279e
Kim Hee Su <wlskaka4@gmail.com>
2024-11-18 20:30:50 +0900

    [Gold V] Title: 캡틴 이다솜, Time: 176 ms, Memory: 15312 KB -BaekjoonHub

--- 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.java
+++ 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.java
@@ -1,37 +1,42 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
-
-        // 사면체 수 리스트 생성
-        ArrayList<Integer> tetrahedralNumbers = new ArrayList<>();
-        int num = 1;
-        int i = 1;
-        while (num <= N) {
-            tetrahedralNumbers.add(num);
-            i++;
-            num = i * (i + 1) * (i + 2) / 6;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        init(n);
+        
+        dp = new int [n+1];
+        Arrays.fill(dp, INT_MAX);
+        
+        dp[0] = 0;
+        for(int t : tetras) {
+        	for(int i = t; i<=n; i++) {
+        		if(dp[i - t] != INT_MAX)
+        			dp[i] = Math.min(dp[i], dp[i - t] + 1);
+        	}
         }
-
-        // DP 배열 초기화
-        int[] dp = new int[N + 1];
-        for (i = 1; i <= N; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
-
-        // DP를 이용한 최소 개수 계산
-        for (int t : tetrahedralNumbers) {
-            for (i = t; i <= N; i++) {
-                if (dp[i - t] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[i - t] + 1);
-                }
-            }
-        }
-
-        System.out.println(dp[N]);
+        
+        System.out.println(dp[n]);
     }
+    
+    
+    static int dp[];
+    static void init(int n) {
+    	int num = 1;
+    	int idx = 1;
+    	
+    	while(num <= n) {
+    		tetras.add(num);
+    		idx++;
+    		num = idx * (idx + 1) * (idx + 2) / 6;
+    	}
+    		
+    }
+    static List<Integer> tetras = new ArrayList<>();
+    
+    static int INT_MAX = Integer.MAX_VALUE;
+    static int ans = INT_MAX;
+    static int n;
 }
