fa7e424dd107de453ecc663abfe8ccebd6ecb2dd
Kim Hee Su <wlskaka4@gmail.com>
2024-12-02 12:43:18 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -1,56 +1,63 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int n, k;
-    static int[] arr = new int[104];
-    static int[] visited = new int[104];
-    static int cnt = 0;
+    static int[] arr;
+    static boolean[] visited;
+    static int cnt;
     static final int INF = 0x7f7f7f7f;
+    static StringTokenizer st;
+    
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-        k = sc.nextInt();
-        n = sc.nextInt();
-
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Integer> v = new ArrayList<>();
+        visited = new boolean[n + 1];
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            if (visited[arr[i]] == 1) {
+            if (visited[arr[i]]) {
                 continue;
             }
 
-            if (v.size() == k) {
-                int last_idx = 0;
+            if (list.size() == k) {
+                int last = 0;
                 int pos = -1;
 
-                for (int a : v) {
-                    int here_pick = INF;
+                for (int e : list) {
+                    int here = INF;
 
                     for (int j = i + 1; j < n; j++) {
-                        if (a == arr[j]) {
-                            here_pick = j;
+                        if (e == arr[j]) {
+                            here = j;
                             break;
                         }
                     }
 
-                    if (last_idx < here_pick) {
-                        last_idx = here_pick;
-                        pos = a;
+                    if (last < here) {
+                        last = here;
+                        pos = e;
                     }
                 }
 
-                visited[pos] = 0;
+                visited[pos] = false;
                 cnt += 1;
-                v.remove(Integer.valueOf(pos));  // List에서 해당 값 제거
+                list.remove(Integer.valueOf(pos));  
             }
 
-            v.add(arr[i]);
-            visited[arr[i]] = 1;
+            list.add(arr[i]);
+            visited[arr[i]] = true;
         }
 
         System.out.println(cnt);
