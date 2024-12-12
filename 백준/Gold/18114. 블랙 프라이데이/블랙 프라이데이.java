23d276d696b63f17c5116aaffebdac7b39e357cd
Kim Hee Su <wlskaka4@gmail.com>
2024-12-12 11:00:32 +0900

    [Gold V] Title: 블랙 프라이데이, Time: 168 ms, Memory: 17088 KB -BaekjoonHub

--- 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
+++ 백준/Gold/18114. 블랙 프라이데이/블랙 프라이데이.java
@@ -8,11 +8,10 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         c = Long.parseLong(st.nextToken());
         arr = new long[n];
-        
-        
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n;i++) {
         	arr[i] = Long.parseLong(st.nextToken());
+        	
         	if(arr[i] == c) {
         		System.out.println(1);
         		System.exit(0);
@@ -22,38 +21,51 @@ public class Main {
         
         Arrays.sort(arr);
         
-        for(int i = 0; i < n ;i++) {
-        	for(int j = i + 1; j <n ;j++) {
-        		long sum = arr[i] + arr[j];
-        		
-        		if(sum == c) {
+        int l = 0;
+        int r = n - 1;
+        
+        while(l < r) {
+        	
+        	long sum = arr[l] + arr[r];
+        	
+        	if(sum == c) {
+        		System.out.println(1);
+        		System.exit(0);
+        	}else if(sum < c) {
+        		if(binarySearch(l,r,c - sum)) {
         			System.out.println(1);
             		System.exit(0);
         		}
-        		
-        		int l = 0;
-        		int r = n - 1;
-        		
-        		while(l<=r) {
-        			int mid = (l + r) / 2;
-        			
-        			if(sum + arr[mid] == c) {
-        				if(mid !=i && mid != j) {
-        					System.out.println(1);
-        	        		System.exit(0);
-        				}
-        			}
-        			
-        			if(sum + arr[mid] > c) {
-        				r = mid - 1;
-        			}else 
-        				l = mid + 1;
-        		}
+        		l++;
+        	}else {
+        		r--;
         	}
         }
-        
-        
         System.out.println(0);
+    }
+    
+    static boolean binarySearch(int l, int r, long value) {
+    	int left = l;
+    	int right = r;
+    	
+    	
+    	while(left <= right) {
+    		int mid = (left+right) / 2;
+    		
+    		if(arr[mid] == value) {
+    			if(mid != l && mid != r)
+    				return true;
+    			return false;
+    		}
+    		else if(arr[mid] > value) {
+    			right = mid - 1;
+    		}else {
+    			left = mid + 1;
+    		}
+    		
+    	}
+    	
+    	return false;
     }
     
     static StringTokenizer st;
