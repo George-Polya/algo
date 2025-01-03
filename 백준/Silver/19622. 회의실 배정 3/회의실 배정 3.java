46b51b56d361d9aba98b2982d6c87830975a99d2
Kim Hee Su <wlskaka4@gmail.com>
2025-01-03 23:18:14 +0900

    [Silver II] Title: 회의실 배정 3, Time: 348 ms, Memory: 50172 KB -BaekjoonHub

--- 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.java
+++ 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.java
@@ -6,52 +6,29 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		meetings = new Meeting[n + 1];
+		meetings = new int[n + 1][3];
 		for(int i = 0 ;i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			int cnt = Integer.parseInt(st.nextToken());
-			meetings[i] = new Meeting(start, end, cnt);
+			meetings[i] = new int[] {start, end, cnt};
 			
 		}
 		
-		
-		dp = new int[n];
-		int ans = -1;
+		dp = new int[n + 1];
 		
 		
-		dp[0] = meetings[0].cnt;
-		if(n >= 1)
-			ans = dp[0];
-		if(n >= 2)
-			dp[1] = meetings[1].cnt;
+		dp[0] = meetings[0][2];
+		dp[1] = Math.max(dp[0], meetings[1][2]);
 		
-		
-		for(int i = 2; i < n ;i++) {
-			dp[i] = meetings[i].cnt + ans;
-			ans = Math.max(ans,  dp[i-1]);
+		for(int i = 2; i<n; i++) {
+			dp[i] = Math.max(dp[i-2] + meetings[i][2], dp[i-1]);
 		}
-		
-		ans = Math.max(ans,  dp[n-1]);
-		
-		System.out.println(ans);
+		System.out.println(dp[n-1]);
 	}
 	static int dp[];
 	static int n;
 	static StringTokenizer st;
-	static Meeting meetings[];
-	static class Meeting implements Comparable<Meeting>{
-		int start, end, cnt;
-		public Meeting(int start, int end, int cnt) {
-			this.start = start;
-			this.end = end;
-			this.cnt = cnt;
-		}
-		
-		public int compareTo(Meeting o) {
-			return end - o.end;
-		}
-	}
-	
+	static int meetings[][];
 }
