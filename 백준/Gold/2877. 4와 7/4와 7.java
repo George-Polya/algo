60deeae460d88d8614e624b1637872f53a4fc819
Kim Hee Su <wlskaka4@gmail.com>
2025-01-19 10:50:08 +0900

    [Gold V] Title: 4와 7, Time: 124 ms, Memory: 16088 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.java
+++ 백준/Gold/2877. 4와 7/4와 7.java
@@ -5,11 +5,14 @@ public class Main {
     public static void main(String[] args) throws IOException{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        k = Integer.parseInt(br.readLine());
-       String s = Integer.toBinaryString(k + 1).replace('0', '4').replace('1', '7');
-       String ans="";
-       for(int i = 1; i < s.length();i++)
-    	   ans += s.charAt(i);
+       String str = Integer.toBinaryString(k+1);
+       String ans = "";
+       for(int i = 1; i < str.length();i++) {
+    	   ans += str.charAt(i) == '0' ? '4' : '7';
+       }
+//       System.out.println(str+" "+ans);
        System.out.println(ans);
+    		  
     }
     static int k;
 }
