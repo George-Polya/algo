fb234a249f09fdfc895b937ad3385947ae6d2600
Kim Hee Su <wlskaka4@gmail.com>
2025-01-24 16:36:42 +0900

    [Gold III] Title: 한 번 남았다, Time: 120 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Gold/13317. 한 번 남았다/한 번 남았다.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+//    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println(50+" "+49);
+    	for(int i = 49; i>0; i--) {
+    		System.out.printf("%d %d %d\n", i,i+1,-1);
+    	}
+    	
+    }    
+}
