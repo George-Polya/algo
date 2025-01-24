820a4756485129e76a8c74b1583dd3dc27b8a50d
Kim Hee Su <wlskaka4@gmail.com>
2025-01-25 08:44:47 +0900

    [Gold IV] Title: 고층 건물, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.java
+++ 백준/Gold/1027. 고층 건물/고층 건물.java
@@ -1,54 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int[] map;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        map = new int[N];
-        for(int i=0; i<N; i++)
-        {
-            map[i] = Integer.parseInt(st.nextToken());
-        }
-        int ans=0;
-        for(int i=0; i<N; i++)
-        {
-            ans = Math.max(ans, count(i));
-        }
-        System.out.println(ans);
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	n = Integer.parseInt(br.readLine());
+    	st = new StringTokenizer(br.readLine());
+    	arr = new int[n+1];
+    	for(int i =1; i<=n; i++) {
+    		arr[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	int ans = 0;
+    	for(int i = 1; i<=n; i++) {
+    		ans = Math.max(ans, calc(i));
+    	}
+    	System.out.println(ans);
+    }   
+    
+    static int calc(int idx) {
+    	int cnt = 0;
+    	double temp = 0;
+    	
+    	double minG = Double.POSITIVE_INFINITY;
+    	double maxG = Double.NEGATIVE_INFINITY;
+    	
+    	for(int l = idx-1; l>=1; l--) {
+    		double slope = (double)(arr[idx] - arr[l]) / (idx - l);
+    		
+    		if(l == idx - 1|| minG > slope) {
+//    			temp = slope;
+    			minG = slope;
+    			cnt++;
+    		}
+    	}
+    	
+    	for(int r = idx + 1; r<=n; r++) {
+    		double slope = (double)(arr[idx] - arr[r]) / (idx - r);
+    		if(r == idx + 1 || slope > maxG) {
+//    			temp = slope;
+    			maxG = slope;
+    			cnt++;
+    		}
+    	}
+    	return cnt;
     }
-    public static int count(int index)
-    {
-        int cnt=0;
-        double temp=0;
-        for(int i=index-1; i>=0; i--)
-        {
-            double slope = (double)(map[index]-map[i])/(index-i);
-
-            if(i==index-1 || temp>slope)
-            {
-                cnt++;
-                temp = slope;
-            }
-        }
-
-        for(int i=index+1; i<N; i++)
-        {
-            double slope = (double)(map[index]-map[i])/(index-i);
-
-            if(i==index+1 || temp<slope)
-            {
-                cnt++;
-                temp = slope;
-            }
-        }
-        return cnt;
-    }
+    
+    static int n;
+    static int arr[];
+    static StringTokenizer st;
 }
