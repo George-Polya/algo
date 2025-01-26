33a5eaf6b96f132da927dbc81afcc80819aeb4d1
Kim Hee Su <wlskaka4@gmail.com>
2025-01-26 14:40:48 +0900

    [Gold II] Title: 수열의 구간 평균, Time: 504 ms, Memory: 56980 KB -BaekjoonHub

---
+++ 백준/Gold/19566. 수열의 구간 평균/수열의 구간 평균.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	st = new StringTokenizer(br.readLine());
+    	arr = new int[N];
+    	for(int i = 0; i < N;i++) {
+    		arr[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	long pSum[] = new long[N];
+    	pSum[0] = arr[0];
+    	for(int i = 1; i<N; i++) {
+    		pSum[i] = pSum[i-1] + arr[i];
+    	}
+    	
+    	long target = 0;
+    	Map<Long, Integer> map = new HashMap<>();
+    	
+    	long ans = 0;
+    	
+    	for(int i = 0; i < N ;i++) {
+    		target += K;
+    		long diff = target - pSum[i];
+    		
+    		if(diff == 0)
+    			ans++;
+    		
+    		int cnt = map.getOrDefault(diff, 0);
+    		ans += cnt;
+    		map.put(diff, cnt+1);
+    	}
+    	
+    	System.out.println(ans);
+    }
+    static int N,K;
+    static int arr[];
+    static StringTokenizer st;
+}
