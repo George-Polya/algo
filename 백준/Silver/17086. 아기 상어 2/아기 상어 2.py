28d3e6a2736e69128f1b1ca99024c47a2c7602f2
Kim Hee Su <wlskaka4@gmail.com>
2025-01-28 21:31:28 +0900

    [Silver II] Title: 아기 상어 2, Time: 144 ms, Memory: 114956 KB -BaekjoonHub

---
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -0,0 +1,51 @@
+from collections import deque, namedtuple
+
+
+def printBoard(board):
+    for y in range(1,N+1):
+        for x in range(1, M+1):
+            print("%3d" % board[y][x], end='')
+        print()
+
+dy = [-1,-1,0,1,1,1,0,-1]
+dx = [0,1,1,1,0,-1,-1,-1]
+
+def OOB(y, x):
+    return y<=0 or y>N or x<=0 or x>M
+
+if __name__ == '__main__':
+    N,M = map(int, input().split())
+    board = [[0 for _ in range(M+1)] for _ in range(N+1) ]
+    dist = [[-1 for _ in range(M+1)] for _ in range(N+1) ]
+
+    q = deque()
+    Pair = namedtuple('Pair', ['y', 'x'])
+
+    for y in range(1, N+1):
+        board[y] = [0] + list(map(int, input().split()))
+        for x in range(1, M+1):
+            if board[y][x] == 1:
+                q.append(Pair(y,x))
+                dist[y][x] = 0
+
+
+    while q:
+        cur = q.popleft()
+
+        for dir in range(8):
+            ny = cur.y + dy[dir]
+            nx = cur.x + dx[dir]
+
+            if OOB(ny,nx) or dist[ny][nx] != -1:
+                continue
+            dist[ny][nx] = dist[cur.y][cur.x] + 1
+            q.append(Pair(ny,nx))
+
+    ans = -1
+    for y in range(1,N+1):
+        for x in range(1, M+1):
+            # print("%3d" % dist[y][x], end='')
+            ans = max(ans, dist[y][x])
+        # print()
+
+    print(ans)
