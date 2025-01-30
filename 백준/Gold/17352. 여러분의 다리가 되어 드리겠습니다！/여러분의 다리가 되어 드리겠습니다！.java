e1aaacca8ec362eacd53e6052970b965effb29c4
Kim Hee Su <wlskaka4@gmail.com>
2025-01-30 12:30:34 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 632 ms, Memory: 96688 KB -BaekjoonHub

--- 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -1,26 +1,28 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int[] uf;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 정점 개수 입력
         uf = new int[N + 1];
 
-        // 초기화
+        // 유니온 파인드 초기화
         for (int i = 1; i <= N; i++) {
             uf[i] = i;
         }
 
-        // 입력 처리 및 유니온 수행
+        // N-2개의 간선 입력 받기
         for (int i = 0; i < N - 2; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             union(u, v);
         }
 
-        // 각 집합의 크기를 저장할 해시맵
+        // 각 컴포넌트 그룹 찾기
         Map<Integer, Integer> hm = new HashMap<>();
 
         for (int i = 1; i <= N; i++) {
@@ -28,13 +30,13 @@ public class Main {
             hm.put(root, hm.getOrDefault(root, 0) + 1);
         }
 
-        // 가장 큰 집합을 찾음
+        // 가장 큰 집합의 대표 찾기
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(hm.entrySet());
         list.sort((a, b) -> Integer.compare(a.getValue(), b.getValue()));
 
-        int idx = list.get(list.size() - 1).getKey();
+        int idx = list.get(list.size() - 1).getKey(); // 가장 큰 그룹의 대표 노드
 
-        // 두 번째 그룹의 대표 노드 찾기
+        // 다른 그룹의 대표 노드 찾기
         for (int i = 1; i <= N; i++) {
             if (find(i) != idx) {
                 System.out.println(idx + " " + i);
@@ -43,13 +45,13 @@ public class Main {
         }
     }
 
-    // 유니온 파인드의 Find 함수 (경로 압축 포함)
+    // 경로 압축을 활용한 Find 함수
     static int find(int x) {
         if (uf[x] == x) return x;
         return uf[x] = find(uf[x]);
     }
 
-    // 유니온 함수
+    // Union 함수
     static void union(int x, int y) {
         x = find(x);
         y = find(y);
