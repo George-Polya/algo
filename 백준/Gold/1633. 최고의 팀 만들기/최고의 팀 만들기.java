0e284a7d9b5e282858fb15668ba0b85da86b9c8a
Kim Hee Su <wlskaka4@gmail.com>
2025-02-03 19:34:54 +0900

    [Gold IV] Title: 최고의 팀 만들기, Time: 112 ms, Memory: 15740 KB -BaekjoonHub

--- 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.java
+++ 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.java
@@ -1,37 +1,75 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
-    public static int[] white;
-    public static int[] black;
-    public static int[][][] dp;
-    public static void main(String args[]) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        white = new int[1001];
-        black = new int[1001];
-        int index = 0;
-        while(scan.hasNextInt()){
-            white[index] = scan.nextInt();
-            black[index] = scan.nextInt();
-
-            index++;
-        }
-        dp = new int[1001][16][16];
-        System.out.println(solution(0, 0, 0, index));
-
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	String s = "";
+    	
+    	while ((s = br.readLine()) != null && !s.isEmpty()) {
+			st = new StringTokenizer(s);
+			whites[n] = Integer.parseInt(st.nextToken());
+			blacks[n] = Integer.parseInt(st.nextToken());		
+			n++;
+		}
+//    	System.out.println(Arrays.toString(whites));
+    	dp = new int[n+1][16][16];
+//    	for(int z = 0; z< n; z++) {
+//    		for(int y = 0; y<=15; y++) {
+//    			for(int x=0; x<=15; x++) {
+//    				dp[z][y][x] = -1;
+//    			}
+//    		}
+//    	}
+    	System.out.println(solve(0,0,0));
     }
-    public static int solution(int i, int wIndex, int bIndex, int N){
-        if(wIndex==15 && bIndex==15) return 0;
-        if(i==N) return 0;
-
-        if(dp[i][wIndex][bIndex]!=0) return dp[i][wIndex][bIndex];
-        //선택 안했을 경우
-        int ans = solution(i+1, wIndex, bIndex, N);
-        //white
-        if(wIndex<15) ans = Math.max(ans, solution(i+1, wIndex+1, bIndex,N)+white[i]);
-        if(bIndex<15) ans = Math.max(ans, solution(i+1, wIndex, bIndex+1, N)+black[i]);
-        //black
-        dp[i][wIndex][bIndex] = ans;
-        return dp[i][wIndex][bIndex];
-    }
+	static int n, ans;
+	
+	static StringTokenizer st;
+	static int dp[][][];
+	static int whites[] = new int[1001];
+	static int blacks[] = new int[1001];
+	
+	static int solve(int cur, int bCnt, int wCnt) {
+		if(bCnt == 15 && wCnt == 15)
+			return 0;
+		
+		if(cur == n)
+			return 0;
+		
+		
+		if(dp[cur][bCnt][wCnt] != 0)
+			return dp[cur][bCnt][wCnt];
+		
+		int ret = solve(cur+1, bCnt, wCnt);
+		if(bCnt < 15)
+			ret = Math.max(ret, solve(cur+1, bCnt+1, wCnt) + blacks[cur]);
+		if(wCnt < 15)
+			ret = Math.max(ret,  solve(cur+1, bCnt, wCnt+1) + whites[cur]);
+		
+		return dp[cur][bCnt][wCnt] = ret;
+	}
+//	
+//	static void solve(int cur, int bCnt, int wCnt, int sum) {
+//		if(bCnt == 15 && wCnt == 15) {
+//			ans = Math.max(ans, sum);
+//			return;
+//		}
+//		
+//		if(cur == n)
+//			return;
+//		
+//		solve(cur+1, bCnt, wCnt);
+//		
+//		if(bCnt < 15)
+//			solve(cur+1, bCnt+1, wCnt, sum + blacks[cur]);
+//		
+//		if(wCnt < 15)
+//			solve(cur+1, bCnt, wCnt+1, sum + whites[cur]);
+//		
+//		
+//			
+//	}
 }
