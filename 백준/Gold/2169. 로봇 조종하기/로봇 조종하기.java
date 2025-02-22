51fa3926df2a22e9a758f6afaf82bddea98aa339
Kim Hee Su <wlskaka4@gmail.com>
2025-02-22 01:58:48 +0000

    [Gold II] Title: 로봇 조종하기, Time: 556 ms, Memory: 88556 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -10,44 +10,44 @@ public class Main {
     	M = Integer.parseInt(st.nextToken());
     	
     	board = new int[N+1][M+1];
-    	dp = new long[N+1][M+1];
-    	for(int y=1; y<=N;y++) {
+    	dp = new int[N+1][M+1];
+    	for(int y=1; y<=N; y++) {
     		st = new StringTokenizer(br.readLine());
     		for(int x=1; x<=M; x++) {
     			board[y][x] = Integer.parseInt(st.nextToken());
+    			dp[y][x] = INT_MIN;
     		}
     	}
     	
     	dp[1][1] = board[1][1];
-    	
-    	for(int x = 2; x<=M; x++)
+    	for(int x=2; x<=M; x++) {
     		dp[1][x] = dp[1][x-1] + board[1][x];
-    	
-    	for(int y=2; y<=N; y++) {
-    		long left[] = new long[M+1];
-    		long right[] = new long[M+1];
-    		
-    		left[1] = dp[y-1][1] + board[y][1];
-    		
-    		for(int x = 2; x<=M; x++)
-    			left[x] = Math.max(left[x-1], dp[y-1][x]) + board[y][x];
-    		
-    		right[M] = dp[y-1][M] + board[y][M];
-    		for(int x = M-1; x>=1; x--)
-    			right[x] = Math.max(right[x+1], dp[y-1][x]) + board[y][x];
-    		
-    		for(int x=1; x<=M; x++)
-    			dp[y][x] = Math.max(left[x], right[x]);
     	}
     	
+    	for(int y=2; y<=N; y++) {
+    		int lefts[] = new int[M+1];
+    		int rights[] = new int[M+1];
+    		
+    		lefts[1] = dp[y-1][1] + board[y][1];
+    		for(int x=2; x<=M; x++) {
+    			lefts[x] = Math.max(lefts[x-1], dp[y-1][x]) + board[y][x];
+    		}
+    		
+    		
+    		rights[M] = dp[y-1][M] + board[y][M];
+    		for(int x = M - 1; x>=1; x--) {
+    			rights[x] = Math.max(rights[x+1], dp[y-1][x]) + board[y][x];
+    		}
+    		
+    		for(int x=1; x<=M; x++)
+    			dp[y][x] = Math.max(lefts[x], rights[x]);
+    		
+    	}
     	System.out.println(dp[N][M]);
+    	
     }
-    static int INT_MIN = Integer.MIN_VALUE / 2;
-    
-    static int ans;
     static int N,M;
     static StringTokenizer st;
-    static int board[][];
-    static long dp[][];
-    
+    static int board[][], dp[][];
+    static int INT_MIN = Integer.MIN_VALUE/2;
 }
