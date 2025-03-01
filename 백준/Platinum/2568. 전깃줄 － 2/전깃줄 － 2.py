b1ecabfcf6573f0ccf9316f2fae92a7f04066486
Kim Hee Su <wlskaka4@gmail.com>
2025-03-01 10:25:10 +0000

    [Platinum V] Title: 전깃줄 - 2, Time: 288 ms, Memory: 122432 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
@@ -1,69 +1,40 @@
 import bisect
-from pprint import pprint
-from copy import deepcopy
-class Line:
-    def __init__(self, id, a,b):
-        self.id = id
-        self.a = a
-        self.b = b
 
-    def __repr__(self):
-        return f"{self.id}: [{self.a}, {self.b}]"
+def solve():
+    n = int(input())
+    lines = []
+    for _ in range(n):
+        a, b = map(int, input().split())
+        lines.append((a, b))
 
-    def __lt__(self, o):
-        return self.a < o.a
+    lines.sort()  # A 전봇대 기준으로 정렬
 
-def lower_bound(arr, target):
-    l = 0
-    r = len(arr) - 1
-    ret = 0
-    while l<=r :
-        mid = (l + r) // 2
-        if target <= arr[mid].b:
-            r = mid - 1
-            ret = mid
+    tails = []  # LIS의 끝 값들을 저장 (증가하는 순서)
+    trace = [-1] * n  # 역추적을 위한 배열
+
+    for i in range(n):
+        a, b = lines[i]
+        if not tails or b > tails[-1]:
+            tails.append(b)
+            trace[i] = len(tails) - 1  # LIS의 몇 번째 위치인지 기록
         else:
-            l = mid + 1
-
-    return ret
-if __name__ == '__main__':
-    N = int(input())
-    lines = [Line(-1,-1,-1)]
-    for i in range(1,N+1):
-        a,b = map(int,input().split())
-        lines.append(Line(i, a,b))
-
-    lines.sort()
-    # print(lines)
-    # path = [0 for _ in range(N + 1)]
-
-    dp = [lines[1]]
-    trace = [-1] * (N+1)
-    trace[1] = 0
-    # print(dp)
-    # print('----')
-    for i in range(2, N+1):
-        if lines[i].b > dp[-1].b:
-            dp.append(lines[i])
-            trace[i] = len(dp) - 1
-        else:
-            idx = lower_bound(dp, lines[i].b)
-            # print("idx: ", cur)
-
-            dp[idx] = lines[i]
+            idx = bisect.bisect_left(tails, b)  # b가 들어갈 위치 (lower bound)
+            tails[idx] = b
             trace[i] = idx
-            # path.append(dp)
 
+    lis_length = len(tails)
+    print(n - lis_length)  # 제거해야 할 전깃줄 개수
 
-    print(N - len(dp))
-    # print(trace)
-    ans = []
-    length = len(dp) - 1
-
-    for i in range(N, 0, -1):
-        if trace[i] == length:
-            length -= 1
+    removed_lines = []
+    lis_index = lis_length - 1
+    for i in range(n - 1, -1, -1):
+        if trace[i] == lis_index:
+            lis_index -= 1
         else:
-            ans.append(lines[i].a)
-    ans.sort()
-    print('\n'.join(map(str,ans)))
+            removed_lines.append(lines[i][0])
+
+    removed_lines.sort()
+    for a in removed_lines:
+        print(a)
+
+solve()
