dad13c27b7ddacae11d4def4e875b248360b2771
Kim Hee Su <wlskaka4@gmail.com>
2025-03-01 10:06:10 +0000

    [Platinum V] Title: 전깃줄 - 2, Time: 616 ms, Memory: 146252 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.py
@@ -0,0 +1,68 @@
+import bisect
+from pprint import pprint
+from copy import deepcopy
+class Line:
+    def __init__(self, id, a,b):
+        self.id = id
+        self.a = a
+        self.b = b
+
+    def __repr__(self):
+        return f"{self.id}: [{self.a}, {self.b}]"
+
+    def __lt__(self, o):
+        return self.a < o.a
+
+def lower_bound(arr, target):
+    l = 0
+    r = len(arr) - 1
+    ret = 0
+    while l<=r :
+        mid = (l + r) // 2
+        if target <= arr[mid].b:
+            r = mid - 1
+            ret = mid
+        else:
+            l = mid + 1
+
+    return ret
+if __name__ == '__main__':
+    N = int(input())
+    lines = []
+    for i in range(1,N+1):
+        a,b = map(int,input().split())
+        lines.append(Line(i, a,b))
+
+    lines.sort()
+    # print(lines)
+    # path = [0 for _ in range(N + 1)]
+
+    dp = []
+    trace = [-1] * (N+1)
+    # print(dp)
+    # print('----')
+    for i in range(N):
+        if not dp or lines[i].b > dp[-1].b:
+            dp.append(lines[i])
+            trace[i] = len(dp) - 1
+        else:
+            idx = lower_bound(dp, lines[i].b)
+            # print("idx: ", cur)
+
+            dp[idx] = lines[i]
+            trace[i] = idx
+            # path.append(dp)
+
+
+    print(N - len(dp))
+
+    ans = []
+    length = len(dp) - 1
+
+    for i in range(N - 1, -1, -1):
+        if trace[i] == length:
+            length -= 1
+        else:
+            ans.append(lines[i].a)
+    ans.sort()
+    print('\n'.join(map(str,ans)))
