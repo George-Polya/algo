908079cda528e4b0dd5deec3d2fd32b08791dacc
Kim Hee Su <wlskaka4@gmail.com>
2025-03-22 02:30:57 +0000

    [Gold III] Title: 수열과 쿼리 15, Time: 360 ms, Memory: 143772 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
@@ -11,32 +11,34 @@ class Pair:
         return self.idx < o.idx
 
     def __repr__(self):
-        return f"idx: {self.idx}, value:{self.value}"
+        return f"idx: {self.idx}, value: {self.value}"
 
-if __name__ == '__main__':
+if __name__=='__main__':
     N = int(input())
-    arr = [0] * (N+1)
-    pq = []
-    _list = list(map(int,input().split()))
-    for i in range(1,N+1):
-        arr[i] = _list[i-1]
-        heappush(pq, Pair(i, arr[i]))
 
+    pq = []
+    arr = [0]
+    for idx, value in enumerate(map(int,input().split())):
+        arr.append(value)
+        heappush(pq, Pair(idx+1, value))
+
+    # print(arr)
     M = int(input())
     ans = []
-    for i in range(M):
-        cmds = list(map(int,input().split()))
-
-        if cmds[0] == 1:
-            idx, value = cmds[1:]
+    for _ in range(M):
+        line = input().split()
+        cmd = int(line[0])
+        if cmd == 1:
+            idx, value = map(int, line[1:])
             arr[idx] = value
             heappush(pq, Pair(idx, value))
-        else:
+        elif cmd == 2:
             while pq:
-                cur = heappop(pq)
-                if arr[cur.idx] == cur.value:
-                    ans.append(cur.idx)
-                    heappush(pq, cur)
+                cur = pq[0]
+                if arr[cur.idx] != cur.value:
+                    heappop(pq)
+                else:
+                    ans.append(str(cur.idx))
                     break
 
-    print('\n'.join(map(str, ans)))
+    print('\n'.join(ans))
