91c7e1decc06cb4990f45077e5b29845643a1cf8
Kim Hee Su <wlskaka4@gmail.com>
2025-05-11 15:03:40 +0900

    [Gold V] Title: 도로의 개수, Time: 100 ms, Memory: 110844 KB -BaekjoonHub

--- 백준/Gold/1577. 도로의 개수/도로의 개수.py
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.py
@@ -1,50 +1,67 @@
-from collections import namedtuple
 import sys
-# sys.setrecursionlimit(10000)
-Pair = namedtuple('Pair', ['x','y'])
-dx = [0,1]
-dy = [1,0]
 
-def isUsed(cur, nxt):
-    tmp = (cur.x, cur.y, nxt.x, nxt.y)
+def solve_baekjoon_1577_bottom_up():
+    # 입력 처리
+    input_line = sys.stdin.readline
+    
+    N_val, M_val = map(int, input_line().split())
+    K = int(input_line())
+    
+    broken_roads = set()
+    for _ in range(K):
+        a, b, c, d = map(int, input_line().split())
+        # 공사 중인 도로를 frozenset으로 저장
+        # (a,b)와 (c,d)는 인접한 두 점
+        point1 = (a, b)
+        point2 = (c, d)
+        broken_roads.add(frozenset({point1, point2}))
+        
+    # DP 테이블 초기화: dp[r][c]는 (0,0)에서 (r,c)까지 가는 경로의 수
+    # N_val이 가로(x), M_val이 세로(y)
+    dp = [[0] * (M_val + 1) for _ in range(N_val + 1)]
+    
+    # 기본 경우: (0,0)에 도달하는 방법은 1가지
+    # 이 값을 설정하지 않으면, dp[0][0]에서 시작하는 경로가 계산되지 않음.
+    # 하지만 아래 반복문에서 dp[r][c] += ... 형태로 누적하므로,
+    # dp[0][0]이 0으로 시작하면 (0,0)에서 출발하는 경로가 제대로 전파되지 않음.
+    # 따라서 dp[0][0] = 1로 설정하고, 반복문 내에서는 (0,0)을 특별 취급하지 않아도 됨.
+    # 또는, 반복문 내에서 (r,c) == (0,0)일 때 dp[0][0] = 1로 설정하고 continue.
+    # 여기서는 dp[0][0] = 1로 미리 설정하는 방식을 사용.
+    
+    dp[0][0] = 1 # 시작점 설정
 
-    return (cur.x, cur.y, nxt.x, nxt.y) in used
+    # DP 테이블 채우기
+    for r in range(N_val + 1):  # x 좌표 (가로)
+        for c in range(M_val + 1): # y 좌표 (세로)
+            
+            # (0,0)은 이미 1로 설정되어 있고, 아래 로직은 (0,0)으로 '오는' 경로를 더하는 것이므로
+            # dp[0][0]에 대해서는 아래 if문들이 실행되지 않거나, 실행되어도 dp[-1][0] 등을 참조하지 않음.
+            # 만약 dp[0][0]을 1로 설정하지 않고, 여기서 (r,c)==(0,0)일때 1을 할당하고 continue하면,
+            # 다른 셀들이 dp[0][0]을 참조할 때 문제가 생길 수 있음.
+            # 따라서 dp[0][0]=1을 먼저 설정하고, 모든 셀에 대해 동일한 로직으로 이전 셀로부터 값을 받아오는 것이 좋음.
 
-def OOB(nxt):
-    return nxt.x > N or nxt.y > M
+            # 현재 셀 (r,c)로 올 수 있는 경로를 계산
+            
+            # 1. 왼쪽에서 오는 경우: (r-1, c)에서 (r, c)로 이동
+            # (r-1, c)가 유효하고, 해당 도로가 공사 중이 아니어야 함.
+            if r > 0: # (r-1, c)가 격자 내에 있는지 확인
+                road_from_left = frozenset({(r - 1, c), (r, c)})
+                if road_from_left not in broken_roads:
+                    dp[r][c] += dp[r - 1][c]
+            
+            # 2. 아래쪽에서 오는 경우: (r, c-1)에서 (r, c)로 이동
+            # (r, c-1)가 유효하고, 해당 도로가 공사 중이 아니어야 함.
+            if c > 0: # (r, c-1)가 격자 내에 있는지 확인
+                road_from_down = frozenset({(r, c - 1), (r, c)})
+                if road_from_down not in broken_roads:
+                    dp[r][c] += dp[r][c - 1]
+            
+            # 주의: dp[0][0]의 경우, r>0과 c>0 조건이 모두 false이므로 아무것도 더해지지 않음.
+            # 만약 dp[0][0]을 0으로 초기화했다면, 여기서 dp[0][0]은 0으로 남게 됨.
+            # 그래서 dp[0][0]=1 초기화가 중요.
 
-def solve(x, y):
-    if x > N or y > M:
-        return 0
-    if x == N and y == M:
-        return 1
-
-    if (x,y) in dp:
-        return dp[(x,y)]
-
-    ret = 0
-    right = frozenset({(x+1,y), (x,y)})
-    if right not in used:
-        ret += solve(x+1,y)
-
-
-    up = frozenset({(x,y+1), (x,y) })
-    if up not in used:
-        ret += solve(x, y+1)
-
-    dp[(x,y)] = ret
-    return ret
-
-
-
+    # 최종 결과 출력
+    print(dp[N_val][M_val])
 
 if __name__ == "__main__":
-    N,M = map(int,input().split())
-    K = int(input())
-    used = set()
-    for _ in range(K):
-        x1,y1,x2,y2 = map(int,input().split())
-        used.add(frozenset({(x1,y1), (x2,y2)}))
-
-    dp = {}
-    print(solve(0,0))
+    solve_baekjoon_1577_bottom_up()
