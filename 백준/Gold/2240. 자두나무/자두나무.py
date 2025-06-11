7678f82017e2e91e51d64f07c10df3f726d7a971
Kim Hee Su <wlskaka4@gmail.com>
2025-06-11 19:38:47 +0900

    [Gold IV] Title: 자두나무, Time: 140 ms, Memory: 115276 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+def solve(idx, cnt):
+    if cnt > W:
+        return -float('inf')
+
+    if idx > T:
+        return 0
+
+    if (idx, cnt) in dp:
+        return dp[(idx,cnt)]
+
+    pos = cnt % 2 + 1
+
+    plum = 1 if pos == arr[idx] else 0
+
+    dp[(idx,cnt)] = max(solve(idx + 1, cnt), solve(idx + 1, cnt + 1)) + plum
+    return dp[(idx,cnt)]
+
+if __name__ == "__main__":
+    T,W = map(int,input().split())
+    arr = [0]
+
+    for _ in range(T):
+        arr.append(int(input()))
+
+    dp = defaultdict(int)
+
+
+    print(max(solve(1,0), solve(1,1)))
