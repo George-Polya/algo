023722c6daf4904494017b84d64b3c7168bcb582
Kim Hee Su <wlskaka4@gmail.com>
2025-06-16 20:10:53 +0900

    [Gold IV] Title: 인접한 비트의 개수, Time: 108 ms, Memory: 112908 KB -BaekjoonHub

---
+++ 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.py
@@ -0,0 +1,33 @@
+
+
+if __name__ == "__main__":
+    T = int(input())
+    MAX_K = 99
+    MAX_N = 100
+    dp = dict()
+    ans = []
+    for _ in range(T):
+        N, K = map(int,input().split())
+
+        def solve(idx, cnt, bit):
+            if cnt < 0 :
+                return 0
+
+            if idx == 1:
+                return 1 if cnt == 0 else 0
+
+            if (idx, cnt, bit) in dp:
+                return dp[(idx,cnt, bit)]
+
+            ret = solve(idx - 1, cnt, 0)
+            if bit == 0:
+                ret += solve(idx -1, cnt, 1)
+            else:
+                ret += solve(idx - 1, cnt - 1, 1)
+
+            dp[(idx,cnt,bit)] = ret
+            return ret
+
+        ans.append(solve(N, K, 0) + solve(N,K,1))
+
+    print('\n'.join(map(str, ans)))
