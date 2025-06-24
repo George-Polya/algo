80ce6676b452bbe6b3eec31a8235848bcfa6dd22
Kim Hee Su <wlskaka4@gmail.com>
2025-06-24 21:08:22 +0900

    [Gold III] Title: 거울 설치, Time: 188 ms, Memory: 116732 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -1,107 +1,70 @@
-from collections import namedtuple, deque
+from collections import namedtuple
 from heapq import heappush, heappop
-from pprint import pprint
-
-dy = [-1,1,0,0]
-dx = [0,0,-1,1]
-
-def OOB(y, x):
-    return y< 0 or y>=N or x<0 or x>=N
-
-def findStart(start):
-    for dir in range(4):
-        ny = start[0] + dy[dir]
-        nx = start[1] + dx[dir]
-
-        if OOB(ny,nx) or board[ny][nx] == '*':
-            continue
-        return (ny,nx,dir)
-
-    return None
-
-def getNxtDirs(dir):
-    if dir == 0 or dir == 1:
-        return [2,3]
-
-    if dir == 2 or dir == 3:
-        return [0,1]
-
 
 class State:
-    def __init__(self, y,x,dir, cnt):
+    def __init__(self,y,x,d,cnt):
         self.y = y
         self.x = x
-        self.dir = dir
+        self.d = d
         self.cnt = cnt
 
     def __lt__(self, o):
         return self.cnt < o.cnt
 
-    def __repr__(self):
-        return f"y: {self.y}, x: {self.x}, dir: {self.dir}, cnt: {self.cnt}"
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+def OOB(y,x):
+    return y<0 or y>= N or x<0 or x>=N
 
-if __name__ == '__main__':
+def getNxtDirs(d):
+    if d == 0 or d == 1:
+        return [2,3]
+    return [0,1]
+
+if __name__ == "__main__":
     N = int(input())
-    board = [[] for _ in range(N)]
 
-    start = None
-    end = None
+    board = [[] for _ in range(N)]
+    start = end = None
     for y in range(N):
         line = input()
-        for x in range(len(line)):
+        for x in range(N):
             board[y].append(line[x])
             if board[y][x] == '#':
                 if start == None:
                     start = (y,x)
-                elif end == None:
+                else:
                     end = (y,x)
 
 
 
-    # pprint(board)
-    # print("start", start)
-    # print("end", end)
+    pq = []
+    visited = [ [[False for _ in range(N)] for _ in range(N)]for _ in range(4)]
+    for d in range(4):
+        heappush(pq, State(start[0], start[1], d, 0))
 
-    # State = namedtuple('State', ['y', 'x','dir','cnt'])
+    while pq:
+        cur : State = heappop(pq)
+        visited[cur.d][cur.y][cur.x] = True
 
-    # start = findStart(start)
-
-    # pprint(start)
-    # q = deque()
-    q = []
-    for dir in range(4):
-        state = State(start[0]+dy[dir], start[1]+dx[dir], dir, 0)
-        heappush(q, state)
-
-    # pprint(q)
-    while q:
-        # cur = q.popleft()
-        cur = heappop(q)
-        # print("cur: ", cur)
-
-        if OOB(cur.y,cur.x):
-            continue
-
-        # pprint(cur)
-        if board[cur.y][cur.x] == '.':
-            ny = cur.y + dy[cur.dir]
-            nx = cur.x + dx[cur.dir]
-            heappush(q, State(ny,nx,cur.dir, cur.cnt))
-            # q.append(State(ny,nx,cur.dir, cur.cnt))
-
-        elif board[cur.y][cur.x] == '!':
-            ny = cur.y + dy[cur.dir]
-            nx = cur.x + dx[cur.dir]
-            heappush(q, State(ny,nx,cur.dir, cur.cnt))
-            # q.append(State(ny, nx, cur.dir, cur.cnt))
-
-            nxtDirs = getNxtDirs(cur.dir)
-            for nDir in nxtDirs:
-                ny = cur.y + dy[nDir]
-                nx = cur.x + dx[nDir]
-                heappush(q, State(ny,nx,nDir, cur.cnt+1))
-                # q.append(State(ny,nx, nDir, cur.cnt+1))
-
-        elif cur.y == end[0] and cur.x == end[1]:
+        if cur.y == end[0] and cur.x == end[1]:
             print(cur.cnt)
-            break
+            exit(0)
+
+        if board[cur.y][cur.x] == '.' or board[cur.y][cur.x] == '#':
+            ny = cur.y + dy[cur.d]
+            nx = cur.x + dx[cur.d]
+            if not OOB(ny,nx) and board[ny][nx] != '*' and not visited[cur.d][ny][nx]:
+                heappush(pq, State(ny,nx,cur.d, cur.cnt))
+        elif board[cur.y][cur.x] == '!':
+            ny = cur.y + dy[cur.d]
+            nx = cur.x + dx[cur.d]
+
+            if not OOB(ny,nx) and board[ny][nx] != '*' and not visited[cur.d][ny][nx]:
+                heappush(pq, State(ny,nx,cur.d, cur.cnt))
+
+            for nd in getNxtDirs(cur.d):
+                ny = cur.y + dy[nd]
+                nx = cur.x + dx[nd]
+                if not OOB(ny,nx) and board[ny][nx] != '*' and not visited[nd][ny][nx]:
+                    heappush(pq, State(ny,nx,nd,cur.cnt + 1))
