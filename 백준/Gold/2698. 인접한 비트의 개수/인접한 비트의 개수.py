79d9159f2b926f974e4f6695e4a6690f249f95cc
Kim Hee Su <wlskaka4@gmail.com>
2025-07-04 20:35:33 +0900

    [Gold IV] Title: 인접한 비트의 개수, Time: 108 ms, Memory: 112844 KB -BaekjoonHub

--- 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.py
+++ 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.py
@@ -1,3 +1,22 @@
+def solve(idx, cnt, bit):
+    global dp
+    if cnt < 0:
+        return 0
+
+    if idx == 1:
+        return 1 if cnt == 0 else 0
+
+    if (idx, cnt, bit) in dp:
+        return dp[(idx, cnt, bit)]
+
+    ret = solve(idx - 1, cnt, 0)
+    if bit == 0:
+        ret += solve(idx - 1, cnt, 1)
+    else:
+        ret += solve(idx - 1, cnt - 1, 1)
+
+    dp[(idx, cnt, bit)] = ret
+    return ret
 
 
 if __name__ == "__main__":
@@ -9,24 +28,6 @@ if __name__ == "__main__":
     for _ in range(T):
         N, K = map(int,input().split())
 
-        def solve(idx, cnt, bit):
-            if cnt < 0 :
-                return 0
-
-            if idx == 1:
-                return 1 if cnt == 0 else 0
-
-            if (idx, cnt, bit) in dp:
-                return dp[(idx,cnt, bit)]
-
-            ret = solve(idx - 1, cnt, 0)
-            if bit == 0:
-                ret += solve(idx -1, cnt, 1)
-            else:
-                ret += solve(idx - 1, cnt - 1, 1)
-
-            dp[(idx,cnt,bit)] = ret
-            return ret
 
         ans.append(solve(N, K, 0) + solve(N,K,1))
 
