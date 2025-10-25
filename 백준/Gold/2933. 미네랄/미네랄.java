1f6b2cbaac76b2fc81f2c4736d8dc52f61a94a0f
Kim Hee Su <wlskaka4@gmail.com>
2025-10-25 16:16:31 +0900

    [Gold I] Title: 미네랄, Time: 792 ms, Memory: 60888 KB -BaekjoonHub

---
+++ 백준/Gold/2933. 미네랄/미네랄.java
@@ -0,0 +1,167 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		board = new char[R+1][C+1];
+		
+		for(int y=1;y<=R; y++) {
+			String line = br.readLine();
+			for(int x=1; x<=C; x++) {
+				board[y][x] = line.charAt(x-1);
+			}
+		}
+		
+		N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N ;i++) {
+			int h = Integer.parseInt(st.nextToken());
+//			System.out.printf("-----i: %d, h: %d-------\n", i,h);
+			h = (R + 1) - h;
+			pitch(h, (i % 2) == 0);
+//			System.out.println("before fall");
+//			printBoard(board);
+			
+			Cluster cluster = makeCluster();
+			if(!cluster.lists.isEmpty()) {
+				cluster.fall();
+			}
+		}
+		printBoard(board);
+	}
+	
+	static Cluster makeCluster() {
+		
+		visited = new boolean[R+1][C+1];
+		
+		for(int x=1; x<=C;x++) {
+			if(visited[R][x])
+				continue;
+			bfs(R,x);
+		}
+		List<Pair> lists = new ArrayList<>();
+		
+		for(int y=1; y<=R; y++) {
+			for(int x=1; x<=C; x++) {
+				if(board[y][x] == 'x' && !visited[y][x])
+					lists.add(new Pair(y,x));
+			}
+		}
+		
+		return new Cluster(lists);
+	}
+	
+	static void bfs(int y, int x){
+		Queue<Pair> q = new ArrayDeque<>();
+		q.add(new Pair(y,x));
+		visited[y][x] = true;
+		while(!q.isEmpty()) {
+			Pair cur = q.poll();
+			
+			for(int dir =0;dir < 4; dir++) {
+				int ny = cur.y + dy[dir];
+				int nx = cur.x + dx[dir];
+				
+				if(OOB(ny,nx) || visited[ny][nx] || board[ny][nx] == '.')
+					continue;
+				q.add(new Pair(ny,nx));
+				visited[ny][nx] = true;
+			}
+		}
+	}
+	
+	static int dy[] = {1,-1,0,0};
+	static int dx[] = {0,0,-1,1};
+	
+	static boolean OOB(int y,int x) {
+		return y<=0 || y > R || x<=0 || x>C;
+	}
+	
+	static boolean visited[][];
+	static class Cluster{
+		List<Pair> lists;
+		public Cluster(List<Pair> lists) {
+			this.lists = lists;
+		}
+		
+		public void fall() {
+			for(Pair p : lists) {
+				board[p.y][p.x] = '.';
+			}
+			
+			for(int i = 1; i<=R; i++) {
+				int nxt = i; 
+				List<Pair> list2 = lists.stream().map(p-> new Pair(p.y + nxt, p.x)).collect(Collectors.toList());
+				if(check(list2)) {
+					for(Pair p : list2) {
+						board[p.y][p.x] = 'x';
+					}
+					break;
+				}
+			}
+		}
+		
+		private boolean check(List<Pair> list) {
+			for(Pair p : list) {
+				int ny = p.y + 1;
+				int nx = p.x;
+				if(OOB(ny,nx) || board[ny][nx] == 'x')
+					return true;
+			}
+			return false;
+		}
+	}
+	
+	static class Pair implements Comparable<Pair>{
+		int y,x;
+		public Pair(int y,int x) {
+			this.y = y;
+			this.x = x;
+		}
+		
+		public int compareTo(Pair o) {
+			return o.y - this.y;
+		}
+		
+		public String toString() {
+			return String.format("(%d, %d)", y,x);
+		}
+	}
+	
+	static void pitch(int y, boolean direction) {
+		if(direction) {
+			for(int x=1; x<=C; x++) {
+				if(board[y][x] == 'x') {
+					board[y][x] = '.';
+					break;
+				}
+			}
+		}else {
+			for(int x = C; x>=1; x--) {
+				if(board[y][x] == 'x') {
+					board[y][x] = '.';
+					break;
+				}
+			}
+		}
+		
+	}
+	
+	static void printBoard(char borad[][]) {
+		for(int y=1; y<=R; y++) {
+			for(int x=1; x<=C; x++) {
+				System.out.printf("%s", board[y][x]);
+			}
+			System.out.println();
+		}
+	}
+	
+	static char board[][];
+	static int R,C,N;
+	static StringTokenizer st;
+}
