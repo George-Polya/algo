03a64ef6f9a40752d17788cd2063c0efaa813a60
Kim Hee Su <wlskaka4@gmail.com>
2025-12-03 20:33:45 +0900

    [Gold IV] Title: 문자열 폭발, Time: 192 ms, Memory: 172548 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -9,15 +9,18 @@ if __name__ == "__main__":
         # while stk[-len(target):] == target:
         #     stk = stk[:-len(target)]
 
-        if len(stk) >= len(target) and stk[-1] == target[-1]:
-            ss = ""
-            for ch in target:
-                ss += stk.pop()
+        if len(stk) >= len(target):
+            isSame = True
+            for i in range(len(target)-1,-1,-1):
+                # if target[i] != stk[]
+                if stk[len(stk) - len(target) + i] != target[i]:
+                    isSame = False
+                    break
 
-            ss = ss[::-1]
-            if target != ss:
-                for c in ss:
-                    stk.append(c)
+            if isSame:
+                for i in range(len(target)):
+                    stk.pop()
+
 
 
     print(''.join(stk) if stk else "FRULA")
